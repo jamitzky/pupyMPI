7158dfc26594198e1036feb69af861f54d71ff55
Rune Bromer <rune.bromer@coniuro.dk>
2009-06-21 14:15:42 +0200

    Adding empty finalize()

--- pupympi/startup.py
+++ pupympi/startup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python2.6
 
+import mpi
 
 def hello_world(from_place, test="hej"):
-    import mpi
     print "hello world. I'm %d of %d (calling from %s)" % (mpi.rank(), mpi.size(), from_place)
 
-import mpi
+    # blank
+    mpi.finalize()
+
 mpi.initialize(15, hello_world, "berlin", test="hej")
 
-# blank
