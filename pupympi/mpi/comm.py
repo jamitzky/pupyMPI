cae179960048bbc8a9cb5ce290df6929dcacf7b2
Rune Bromer <rune.bromer@coniuro.dk>
2009-06-21 13:27:23 +0200

    Added __repr__ to communicator object for easier debugging

--- pupympi/mpi/comm.py
+++ pupympi/mpi/comm.py
@@ -1,5 +1,8 @@
-
 class Communicator():
-    def __init__(self, rank, np):
+    def __init__(self, rank, size, name="MPI_COMM_WORLD"):
         self.rank = rank
-        self.size = np
+        self.size = size
+        self.name = name
+
+    def __repr__(self):
+        return "<Communicator %s with %d members>" % (self.name, self.size)
