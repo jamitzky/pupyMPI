ad6011e97d8a0dddc55e2506f804a0b2c0bb77fd
Rune Bromer <rune.bromer@coniuro.dk>
2009-07-16 13:11:59 +0200

    The system has logging now. For some reason only logger.error() seems to work but this will be fixed shortly

---
+++ pupympi/mpi/logger.py
@@ -0,0 +1,30 @@
+import os
+import logging, logging.handlers
+
+def setup_log(filename, logname, debug, verbosity, quiet):
+    if debug:
+        verbosity = 3
+        
+    verbosity_conversion = [logging.ERROR,logging.WARNING,logging.INFO,logging.DEBUG]
+    
+    level = verbosity_conversion[ verbosity ]
+    _BASE = os.path.dirname(os.path.abspath(__file__))
+    _LOG_BASE = os.path.join(_BASE, '..', '%s.log')
+    filepath = _LOG_BASE % os.path.basename(filename)
+    basename = os.path.splitext(os.path.basename(filename))[0]
+    
+    filelog = logging.FileHandler(filepath)
+    formatter = logging.Formatter('%(asctime)s %(name)-12s: %(levelname)-8s %(message)s', '%Y-%m-%d %H:%M:%S')
+    filelog.setFormatter(formatter)
+    filelog.setLevel(level)
+    logging.getLogger(logname).addHandler(filelog)
+   
+    if not quiet and verbosity > 0:
+        console = logging.StreamHandler()
+        console.setFormatter(formatter)
+        console.setLevel(level)
+        logging.getLogger(logname).addHandler(console)
+        
+    logger = logging.getLogger(logname)
+    
+    return logger
