d17ea1daa0c227baf8f4357a4d65c71c7e6db1ce
Rune Bromer <rune.bromer@coniuro.dk>
2009-08-04 14:06:40 +0200

    Code refactored a bit

--- pupympi/tests/TEST_simple_communication.py
+++ pupympi/tests/TEST_simple_communication.py
@@ -5,21 +5,18 @@
 import mpi, time
 from mpi import tcp
 
-
 mpi = mpi.MPI()
 
 rank = mpi.rank()
 size = mpi.size()
 
-time.sleep(max(2,size-rank))
+SOME_TAG = 13
 
-# Send to own rank + 1
-neighbour = (rank + 1) % size
-content = "Message from rank %d" % (rank)
-print "Rank: %d sending to %d" % (rank,neighbour)
-#tcp.isend(neighbour,content,"Dummy tag here")
-
-print "Sending done rank %d of %d after %d seconds sleep" % (rank, size, size-rank)
+if rank == 0:
+    mpi.send(1, "My message to the other side", SOME_TAG)
+else:
+    msg = mpi.recv(0, SOME_TAG)
+    print "Got message", msg
 
 # Close the sockets down nicely
 mpi.finalize()
