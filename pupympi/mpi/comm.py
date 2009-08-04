4451f8d16773a042a9e52121e24f18f49521a3c8
Rune Bromer <rune.bromer@coniuro.dk>
2009-08-04 10:45:59 +0200

    My old code.

--- pupympi/mpi/comm.py
+++ pupympi/mpi/comm.py
@@ -1,8 +1,9 @@
 class Communicator():
-    def __init__(self, rank, size, name="MPI_COMM_WORLD"):
+    def __init__(self, rank, size, mpi_instance, name="MPI_COMM_WORLD"):
         self.rank = rank
         self.size = size
         self.name = name
+        self.mpi_instance = mpi_instance
 
     def __repr__(self):
         return "<Communicator %s with %d members>" % (self.name, self.size)
