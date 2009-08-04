d49fa3687938b2b962e3826d4641cf36ffc69389
Jan Wiberg <jan@lykke.org>
2009-08-04 10:09:00 +0200

    topology

--- pupympi/mpi/comm.py
+++ pupympi/mpi/comm.py
@@ -1,11 +1,18 @@
+
 class Communicator():
     def __init__(self, rank, size, name="MPI_COMM_WORLD"):
         self.rank = rank
         self.size = size
         self.name = name
+        if not topology:
+            topology = Cartesian([size], [False])
 
     def __repr__(self):
         return "<Communicator %s with %d members>" % (self.name, self.size)
 
     def have_rank(self, rank):
         return rank in self.members
+
+    # FIXME: associate a topology with this communicator. Actual way of doing this TBD.
+    def associate(self, topology):
+        self.topology = topology
