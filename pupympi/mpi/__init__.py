4c68a0aa597545c59f0234b52cda59613fc4b092
Jan Wiberg <jan@lykke.org>
2009-08-04 11:48:32 +0200

    impl. mpi_cart_shift

---
+++ pupympi/mpi/__init__.py
@@ -0,0 +1,80 @@
+__version__ = 0.01
+
+from mpi.comm import Communicator
+
+class MPI:
+
+    def __init__(self):
+        import sys, getopt
+        try:
+            optlist, args = getopt.gnu_getopt(sys.argv[1:], 'dv:ql:s:r:', ['verbosity=','quiet','log-file=','debug','size=','rank=','network-type=','port=','mpirun-conn-port=', 'mpirun-conn-host='])
+        except getopt.GetoptError, err:
+            print str(err)
+
+        debug = False
+        verbosity = 1
+        quiet = False
+        rank = 0
+        size = 0
+        hostname = None
+        port = None
+        
+        logfile = None
+
+        for opt, arg in optlist:
+            if opt in ("-d", "--debug"):
+                debug = True
+
+            if opt in ("-v", "--verbosity"):
+                verbosity = int(arg)
+
+            if opt in ("-q", "--quiet"):
+                quiet = True
+
+            if opt in ("-l", "--log-file"):
+                logfile = arg
+
+            if opt in ("-r", "--rank"):
+                try:
+                    rank = int(arg)
+                except ValueError:
+                    pass
+            if opt in ("-s", "--size"):
+                try:
+                    size = int(arg)
+                except:
+                    pass
+                
+            if opt == "--mpirun-conn-host":
+                hostname = arg
+            if opt == "--mpirun-conn-port":
+                port = int(arg)
+                
+        # Initialise the logger
+        from mpi.logger import setup_log
+        logger = setup_log(logfile or "mpi", "proc-%d" % rank, debug, verbosity, quiet)
+        
+        # Let the communication handle start up if it need to.
+        from mpi.tcp import TCPNetwork
+        
+        logger.debug("Starting the network")
+        port = 6000+rank
+        self.network = network = TCPNetwork(port, logger)
+
+        self.network.handshake(hostname, port)
+
+        self.MPI_COMM_WORLD = Communicator(rank, size)
+        logger.debug("MPI_COMM_WORLD started")
+
+    def rank(self, comm=None):
+        if not comm:
+            comm = self.MPI_COMM_WORLD
+        return comm.rank
+
+    def size(self, comm=None):
+        if not comm:
+            comm = self.MPI_COMM_WORLD
+        return comm.size
+    
+    def finalize(self):
+        self.network.finalize()
---
+++ pupympi/mpi/comm2.py
@@ -0,0 +1,18 @@
+import simplecomm
+
+class Communicator():
+    def __init__(self, rank, size, process_placeholder, name="MPI_COMM_WORLD"):
+        self.rank = rank
+        self.size = size
+        self.name = name
+        self.members = {}
+
+        # Initialize the list with other processes
+        for (rank, process) in process_placeholder['all']:
+            self.members[rank] = process
+
+        self.ego = process_placeholder['self']
+
+    def __repr__(self):
+        return "<Communicator %s with %d members>" % (self.name, self.size)
+
