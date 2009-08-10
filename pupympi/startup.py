da1d5bddabc10f33b79e630eaf7aaddd0d960631
Rune Bromer <rune.bromer@coniuro.dk>
2009-08-10 22:39:41 +0200

    strange

--- pupympi/startup.py
+++ pupympi/startup.py
@@ -7,6 +7,7 @@ mpi = mpi.MPI()
 rank = mpi.MPI_COMM_WORLD.rank()
 size = mpi.MPI_COMM_WORLD.size()
 
+
 print "Closing process process %d of %d after %d seconds sleep" % (rank, size, size-rank)
 
 # Close the sockets down nicely
