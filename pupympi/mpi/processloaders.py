47d588ea1c25b2127d041a943549ec96a36dde57
Jan Wiberg <jan@lykke.org>
2009-08-10 21:22:46 +0200

    now with process termination

--- pupympi/mpi/processloaders.py
+++ pupympi/mpi/processloaders.py
@@ -24,18 +24,29 @@ def popenssh(logger, host, arguments):
     else:
         ssh(logger, host, arguments)
     
+process_list = []
+
 def ssh(logger, host, arguments):
+    global remote_list
     """SSH process starter. Non-loadbalancing."""
     sshexec = ["ssh"] + [host] + ["PYTHONPATH=Documents/DIKU/python-mpi/code/pupympi/"]+ arguments 
     logger.debug("Exec: %s" % (' '.join(sshexec)))
-    p = subprocess.Popen(sshexec, stderr=subprocess.PIPE)
-    errdata = p.communicate()
-    print "SSH PROCESS RETURN:\n",errdata
+    p = subprocess.Popen(sshexec, stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+    process_list.append(p)
+    # errdata = p.communicate()
+    # print "SSH PROCESS RETURN:\n",errdata
+    
     
 def popen(logger, host, arguments):
+    global process_list
     if _islocal(host):
         p = subprocess.Popen(arguments)
+        process_list.append(p)
         return p
     else:
         raise MPIException("This processloader can only start processes on localhost, '%s' specified." % host)
     
+def shutdown(logger):
+    for p in process_list:
+        p.terminate()
+        
