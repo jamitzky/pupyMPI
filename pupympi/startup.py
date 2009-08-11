353a51e070220c430e69cecc74f62d6a7d7a5226
Rune Bromer <rune.bromer@coniuro.dk>
2009-08-11 14:20:43 +0200

    Fixed stderr og stdout problem

--- pupympi/startup.py
+++ pupympi/startup.py
@@ -9,5 +9,16 @@ size = mpi.MPI_COMM_WORLD.size()
 
 print "Started process process %d / %d" % (rank, size)
 
+if rank == 0:
+    try:
+        1/0
+    except Exception,e :
+        import sys
+        sys.stderr.write("Der er sket en fejl")
+
+    print "Completing process process %d / %d" % (rank, size)
+else:
+    1/0
+
 # Close the sockets down nicely
 mpi.finalize()
