35aadd8347a01604776b7778e6727c4fb46f084b
hantho <fhantho@yahoo.com>
2009-08-11 14:25:19 +0200

    Starting threading

---
+++ pupympi/mpi/__init__.py
@@ -0,0 +1,100 @@
+__version__ = 0.01
+
+from mpi.communicator import Communicator
+from mpi.logger import Logger
+import threading
+
+class MPI:
+
+    def __init__(self):
+        import sys, getopt
+        try:
+            optlist, args = getopt.gnu_getopt(sys.argv[1:], 'dv:ql:s:r:', ['verbosity=','quiet','log-file=','debug','size=','rank=','network-type=','port=','mpirun-conn-port=', 'mpirun-conn-host='])
+        except getopt.GetoptError, err:
+            print str(err)
+
+        debug = False
+        verbosity = 1
+        quiet = False
+        rank = 0
+        size = 0
+        hostname = None
+        port = None
+        
+        logfile = None
+        
+        for opt, arg in optlist:
+            if opt in ("-d", "--debug"):
+                debug = True
+
+            if opt in ("-v", "--verbosity"):
+                verbosity = int(arg)
+
+            if opt in ("-q", "--quiet"):
+                quiet = True
+
+            if opt in ("-l", "--log-file"):
+                logfile = arg
+
+            if opt in ("-r", "--rank"):
+                try:
+                    rank = int(arg)
+                except ValueError:
+                    pass
+            if opt in ("-s", "--size"):
+                try:
+                    size = int(arg)
+                except:
+                    pass
+                
+            if opt == "--mpirun-conn-host":
+                mpi_run_hostname = arg
+            if opt == "--mpirun-conn-port":
+                mpi_run_port = int(arg)
+
+        # Initialise the logger
+        logger = Logger(logfile or "mpi", "proc-%d" % rank, debug, verbosity, quiet)
+        logger.debug("WHERE'S MY LOG MSG!?")
+        # Let the communication handle start up if it need to.
+        from mpi.tcp import TCPNetwork as Network
+        logger.debug("WHERE'S MY 2 LOG MSG!?")
+        self.network = Network()
+        #Trying threading
+        #logger.debug("trying to start network")
+        #self.network = Network().start()
+        #logger.debug("started network")
+        
+        self.MPI_COMM_WORLD = Communicator(rank, size, self)
+        self.network = network = Network()
+        self.network.set_start_port( 14000 + rank )
+        logger.debug("Network started")
+
+        all_procs = self.network.handshake(mpi_run_hostname, mpi_run_port, rank)
+        self.MPI_COMM_WORLD.build_world( all_procs )
+        logger.debug("Communicator started")
+    
+    def finalize(self):
+        self.network.finalize()
+
+    def _ensure_comm(self, comm):
+        return comm or self.MPI_COMM_WORLD
+
+    # Some wrapper methods
+    def isend(self, destination, content, tag, comm=None):
+        comm = self._ensure_comm(comm)
+        return self.network.isend(destination, content, tag, comm)
+
+    def send(self, destination, content, tag, comm=None):
+        comm = self._ensure_comm(comm)
+        return self.network.send(destination, content, tag, comm)
+
+    def wait(self, handle):
+        return self.network.wait(handle)
+
+    def recv(self, destination, tag, comm=None):
+        comm = self._ensure_comm(comm)
+        return self.network.recv(destination, tag, comm)
+
+    def irecv(self, destination, tag, comm=None):
+        comm = self._ensure_comm(comm)
+        return self.network.irecv(destination, tag, comm)
---
+++ pupympi/mpi/processloaders.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+processloaders.py
+
+Created by Jan Wiberg on 2009-08-06.
+Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+"""
+
+import sys, os, subprocess
+from exceptions import MPIException
+from mpi.logger import Logger
+
+# TODO Output redirect. Log files?
+
+def _islocal(host):
+    return host == "localhost" or host == "127.0.0.1"
+    
+def popenssh(host, arguments):
+    """Mixed Popen/SSH process starter. Uses Popen for localhost hosts, otherwise ssh"""
+    if _islocal(host):
+        p = popen(host, arguments)
+    else:
+        ssh(host, arguments)
+    
+process_list = []
+
+def ssh(host, arguments):
+    global remote_list
+    logger = Logger()
+    """SSH process starter. Non-loadbalancing."""
+    python_path = os.path.dirname(os.path.abspath(__file__)) + "/../"
+    sshexec = ["ssh"] + [host] + ["PYTHONPATH=" + python_path ]+ arguments 
+    logger.debug("Exec: %s" % (' '.join(sshexec)))
+    p = subprocess.Popen(sshexec, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    process_list.append(p)
+    
+    
+def popen(host, arguments):
+    global process_list
+    logger = Logger()
+
+    if _islocal(host):
+        p = subprocess.Popen(arguments)
+        process_list.append(p)
+        return p
+    else:
+        raise MPIException("This processloader can only start processes on localhost, '%s' specified." % host)
+    
+def gather_io():
+    global process_list
+    logger = Logger()
+    for p in process_list:
+        if p.poll():
+            logger.debug( "Active remote process: %s" % (p, p.returncode))
+        else:
+            out,err = p.communicate()
+            logger.info( "Active remote process: %s\n\tStdout: %s\n\tStderr: %s" % ( p.returncode, out, str(err)))
+
+def shutdown():
+    global process_list
+    logger = Logger()
+    for p in process_list:
+        if p.poll():
+            logger.debug("Killing %s" % p)
+            p.texrminate()
+        
