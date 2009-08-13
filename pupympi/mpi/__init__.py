5d92446cc17aaf0b7d51e2f0307e38347834ab32
Rune Bromer <rune.bromer@coniuro.dk>
2009-08-13 19:48:23 +0200

    Added documentation files for the project. It's a very cool system that allows us to build documentation as we want and include doc-strings from the source code as real code documentation.

---
+++ pupympi/mpi/__init__.py
@@ -0,0 +1,129 @@
+__version__ = 0.01
+
+from optparse import OptionParser, OptionGroup
+import threading
+import sys, getopt
+
+from mpi.communicator import Communicator
+from mpi.logger import Logger
+from mpi.tcp import TCPNetwork as Network
+
+class MPI:
+    """
+    This is the main class that contains most of the public API. Initializing 
+    the MPI system is done by creating an instance of this class. Through a
+    MPI instance a program can interact with other processes through different
+    communicators. 
+
+    NOTE: The MPI instance state is a static class variable, so creating multiple
+    instances will always yield 'the same' instance, much like a singleton design
+    pattern. 
+    """
+
+    def __init__(self):
+        """
+        Initializes the MPI environment. 
+        """
+        parser = OptionParser()
+        parser.add_option('--rank', type='int')
+        parser.add_option('--size', type='int')
+        parser.add_option('--verbosity', type='int')
+        parser.add_option('--debug', action='store_true')
+        parser.add_option('--quiet', action='store_true')
+        parser.add_option('--log-file', dest='logfile', default="mpi")
+        parser.add_option('--network-type', dest='network_type')
+        parser.add_option('--mpirun-conn-port', dest='mpi_conn_port')
+        parser.add_option('--mpirun-conn-host', dest='mpi_conn_host')
+
+        options, args = parser.parse_args()
+
+        # Initialise the logger
+        logger = Logger(options.logfile, "proc-%d" % options.rank, options.debug, options.verbosity, options.quiet)
+        # Let the communication handle start up if it need to.
+
+        logger.debug("Finished all the runtime arguments")
+        
+        self.MPI_COMM_WORLD = Communicator(options.rank, options.size, self)
+        ##Trying threading
+        #from mpi.tcp import ThreadTCPNetwork as Network
+        #logger.debug("trying to start network")
+        #self.network = Network().start()
+
+        self.network = Network()
+        logger.debug("Network started")
+
+        all_procs = self.network.handshake(options.mpi_conn_host, int(options.mpi_conn_port), int(options.rank))
+        self.MPI_COMM_WORLD.build_world( all_procs )
+        logger.debug("Communicator started")
+
+        import sys
+        user_options =[sys.argv[0], ] 
+        user_options.append(sys.argv[sys.argv.index("--")+1:])
+
+        sys.argv = user_options
+
+        # Set a static attribute on the class so we know it's initialised.
+        self.__class__._initialized = True
+        logger.debug("Set the MPI environment to initialised")
+
+    def finalize(self):
+        """
+        Run this last
+        """
+        logger = Logger()
+        self.network.finalize()
+        logger.debug("Network finalized")
+
+    def _ensure_comm(self, comm):
+        return comm or self.MPI_COMM_WORLD
+
+    @classmethod
+    def initialized(cls):
+        """
+        document me
+        """
+        return getattr(cls, '_initialized', False)
+
+    # Some wrapper methods
+    def isend(self, destination, content, tag, comm=None):
+        """
+        document me
+        """
+        comm = self._ensure_comm(comm)
+        return self.network.isend(destination, content, tag, comm)
+
+    def send(self, destination, content, tag, comm=None):
+        """
+        document me
+        """
+        comm = self._ensure_comm(comm)
+        return self.network.send(destination, content, tag, comm)
+
+    def wait(self, handle):
+        """
+        document me
+        """
+        Logger().warn("Non-Implemented method 'wait' called.")
+        return self.network.wait(handle)
+        
+    def barrier(self, comm=None):
+        """
+        document me
+        """
+        comm = self._ensure_comm(comm)
+        Logger().warn("Non-Implemented method 'Barrier' called.")
+        return self.network.barrier(comm)
+
+    def recv(self, destination, tag, comm=None):
+        """
+        document me
+        """
+        comm = self._ensure_comm(comm)
+        return self.network.recv(destination, tag, comm)
+
+    def irecv(self, destination, tag, comm=None):
+        """
+        document me
+        """
+        comm = self._ensure_comm(comm)
+        return self.network.irecv(destination, tag, comm)
