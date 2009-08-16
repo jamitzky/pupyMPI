e80936b2360f732f3d43d1a4ba0a44df39c771f6
hantho <fhantho@yahoo.com>
2009-08-16 23:34:01 +0200

    Argh, hangs on everything,

--- pupympi/mpi/__init__.py
+++ pupympi/mpi/__init__.py
@@ -53,7 +53,6 @@ class MPI:
         # Let the communication handle start up if it need to.
 
         logger.debug("Finished all the runtime arguments")
-        
         self.MPI_COMM_WORLD = Communicator(rank, size, self)
         #Trying threading
         #from mpi.tcp import ThreadTCPNetwork as ThreadNetwork
