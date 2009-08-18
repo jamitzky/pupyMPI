6dfac0a105855eb2670f006456366126b7f2d19c
Jan Wiberg <jan@lykke.org>
2009-08-18 14:50:35 +0200

    b0rked runtests.py

--- pupympi/bin/runtests.py
+++ pupympi/bin/runtests.py
@@ -21,11 +21,11 @@ Read the source, lazy bum.
 # settings
 RUN_COUNT = 2 # MPI processes started...tests can use no more than this number
 TEST_EXECUTION_TIME_GRANULARITY = 0.2 # sleep time between checking if process is dead (also determines gran. of execution time, obviously)
-TEST_MAX_RUNTIME = 15 # max time in seconds that one single test may take.
+TEST_MAX_RUNTIME = 6 # max time in seconds that one single test may take.
 LOG_VERBOSITY = 3
 
 class RunTest(Thread):
-    cmd = "python2.6 bin/mpirun.py -q -c RUN_COUNT -v LOG_VERBOSITY -l PRIMARY_LOG_TEST_TRUNC_NAME tests/TEST_NAME"
+    cmd = "bin/mpirun.py -q -c RUN_COUNT -v LOG_VERBOSITY -l PRIMARY_LOG_TEST_TRUNC_NAME tests/TEST_NAME"
     def __init__(self, test, primary_log):
         Thread.__init__(self)
         self.test = test
