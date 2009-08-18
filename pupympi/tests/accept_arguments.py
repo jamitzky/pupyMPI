d30e7dfed8959dd8126217b5d07e56f79a99195c
Rune Bromer <rune.bromer@coniuro.dk>
2009-08-18 10:42:32 +0200

    minor stuff

--- pupympi/tests/accept_arguments.py
+++ pupympi/tests/accept_arguments.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python2.6
 
-import mpi, time
+import mpi
 
 mpi = mpi.MPI()
-
-rank = mpi.MPI_COMM_WORLD.rank()
-size = mpi.MPI_COMM_WORLD.size()
 
 import sys
 print sys.argv
 
-
-# Close the sockets down nicely
 mpi.finalize()
