520b64e4733a1bf2f97ed881c60c577fee044a07
hantho <fhantho@yahoo.com>
2009-08-18 16:08:35 +0200

    Dunno

---
+++ pupympi/tests/TEST_startup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.6
+
+from mpi import MPI
+import time
+
+mpi = MPI.initialize()
+
+rank = mpi.MPI_COMM_WORLD.rank()
+size = mpi.MPI_COMM_WORLD.size()
+
+print "Started process process %d / %d" % (rank, size)
+
+time.sleep(10)
+time.sleep(5)
+
+# Close the sockets down nicely
+mpi.finalize()
