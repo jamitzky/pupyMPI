c5b378fb0d87189df35737afe26618bf68014a0d
Rune Bromer <rune.bromer@coniuro.dk>
2009-08-25 13:50:48 +0200

    First attempt of a two-loop solution to the TCP network handling. Will not work yet, as we don't send the data in the right format.

---
+++ pupympi/mpi/processloaders.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python2.6
+# encoding: utf-8
+"""
+processloaders.py
+
+Created by Jan Wiberg on 2009-08-06.
+Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+"""
+
+import sys, os, subprocess, select, time
+from mpi.exceptions import MPIException
+from mpi.logger import Logger
+
+# TODO Output redirect. Log files?
+
+def _islocal(host):
+    return host == "localhost" or host == "127.0.0.1"
+    
+def popenssh(host, arguments):
+    """Mixed Popen/SSH process starter. Uses Popen for localhost hosts, otherwise ssh"""
+    if _islocal(host):
+        p = popen(host, arguments)
+    else:
+        ssh(host, arguments)
+    
+process_list = []
+
+def ssh(host, arguments):
+    global remote_list
+    logger = Logger()
+    """SSH process starter. Non-loadbalancing."""
+    python_path = os.path.dirname(os.path.abspath(__file__)) + "/../"
+    sshexec = ["ssh"] + [host] + ["PYTHONPATH=" + python_path ]+ arguments 
+    logger.debug("Exec: %s" % (' '.join(sshexec)))
+    p = subprocess.Popen(sshexec, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process_list.append(p)
+    return p
+    
+def popen(host, arguments):
+    global process_list
+    logger = Logger()
+
+    if _islocal(host):
+        p = subprocess.Popen(arguments, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process_list.append(p)
+        return p
+    else:
+        raise MPIException("This processloader can only start processes on localhost, '%s' specified." % host)
+
+def wait_for_shutdown(process_list):
+    logger = Logger()
+    while process_list:
+        for p in process_list:
+            returncode = p.poll()
+            logger.debug("Got return code: %s" % returncode)
+
+            if returncode is None:
+                logger.debug("Process was None, so it was just removed from the process list")
+                process_list.remove( p )
+            elif returncode == 0:
+                logger.debug("Process exited with a status of 0. Frederik can't code for shit.. stupido")
+                process_list.remove( p )
+            else:
+                logger.debug("Process exited with status: %d" % returncode)
+
+        time.sleep(1)
---
+++ pupympi/mpi/request.py
@@ -0,0 +1,112 @@
+from mpi.exceptions import MPIException
+from mpi.logger import Logger
+import threading, time
+
+class Request:
+
+    def __init__(self, type, communicator, participant, tag, data=None):
+        if type not in ('send','receive'):
+            raise MPIException("Invalid type in request creation. This should never happen. ")
+
+        self.type = type
+        self.communicator = communicator
+        self.participant = participant
+        self.tag = tag
+        self.data = data
+
+        # Meta information we use to keep track of what is going on. There are some different
+        # status a request object can be in:
+        # 'new' ->       The object is newly created. If this is send the lower layer can start to 
+        #                do stuff with the data
+        # 'cancelled' -> The user cancelled the request. A some later point this will be removed
+        # 'ready'     -> Means we have the data (in receive) or picked the data (send) and can
+        #                safely return from a test or wait call.
+        self._m = {'status' : 'new' }
+
+        # Start a lock for this request object. The lock should be taken
+        # whenever we change the content. It should be legal to read 
+        # information without locking (like test()). We implement the release() and
+        # acquire function on this class directly so the variabel stays private
+        self._m['lock'] = threading.Lock()
+
+        Logger().debug("Request object created for communicator %s, tag %s and type %s and participant %s" % (self.communicator.name, self.tag, self.type, self.participant))
+
+        # Start the netwok layer on a job as well
+        self.communicator.network.start_job(self, self.communicator, type, self.participant, tag, data)
+
+    def update(self, status=None, data=None, lock=True):
+        if status:
+            if lock:
+                self.acquire()
+            self._m['status'] = status
+
+            if data:
+                self.data = data
+
+            if lock:
+                self.release()
+            Logger().info("%s request objects updated to status %s" % (self.type, self._m['status']))
+
+    def release(self, *args, **kwargs):
+        """
+        Just forwarding method call to the internal lock
+        """
+        return self._m['lock'].release(*args, **kwargs)
+
+    def acquire(self, *args, **kwargs):
+        """
+        Just forwarding method call to the internal lock
+        """
+        return self._m['lock'].acquire(*args, **kwargs)
+
+    def cancel(self):
+        """
+        Cancel a request. This can be used to free memory, but the request must be redone
+        by all parties involved.
+        
+        http://www.mpi-forum.org/docs/mpi-11-html/node50.html
+        """
+        # We just set a status and return right away. What needs to happen can be done
+        # at a later point
+        self._m['status'] = 'cancelled'
+        Logger().debug("Cancelling a %s request" % self.type)
+        
+
+    def wait(self):
+        """
+        Blocks until the request data can be garbage collected. This method can be used
+        to create stable methods limiting the memory usage by not creating new send
+        requests before the ressources for the current one has been removed.
+
+        When waiting for a receive, the data will be returned to the calling function.
+
+        On successfull completing the ressources occupied by this request object will
+        be garbage collected.
+
+        FIXME: This should properly be a bit more thread safe. Should we add a lock to 
+               each request object and lock it when we look at it?
+        """
+        Logger().info("Starting a %s wait" % self.type)
+        while not self.test():
+            time.sleep(1)
+
+        # We're done at this point. Set the request to be completed so it can be removed
+        # later.
+        self._m['status'] = 'finished'
+
+        # Return none or the data
+        if self.type == 'receive':
+            return self.data
+
+        Logger().info("Ending a %s wait" % self.type)
+
+    def test(self):
+        """
+        A non-blocking check to see if the request is ready to complete. If true a 
+        following wait() should return very fast.
+        """
+        Logger().debug("Testing a %s request" % self.type)
+        return self._m['status'] == 'ready'
+
+    def get_status(self):
+        return self._m['status']
