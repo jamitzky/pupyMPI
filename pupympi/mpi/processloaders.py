3bc0f63172bae245060bc12a41ea8338b3da3681
hantho <fhantho@yahoo.com>
2009-08-25 23:35:17 +0200

    More commenting and small edits

--- pupympi/mpi/processloaders.py
+++ pupympi/mpi/processloaders.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python2.6
-# encoding: utf-8
-"""
-processloaders.py
-
-Created by Jan Wiberg on 2009-08-06.
-Copyright (c) 2009 __MyCompanyName__. All rights reserved.
-"""
-
 import sys, os, subprocess, select, time
 from mpi.exceptions import MPIException
 from mpi.logger import Logger
@@ -17,27 +8,29 @@ def _islocal(host):
     return host == "localhost" or host == "127.0.0.1"
     
 def popenssh(host, arguments):
-    """Mixed Popen/SSH process starter. Uses Popen for localhost hosts, otherwise ssh"""
+    """ Mixed Popen/SSH process starter. Uses Popen for localhost, otherwise SSH"""
     if _islocal(host):
         p = popen(host, arguments)
     else:
         ssh(host, arguments)
-    
+
+# Let everyone see the same processes
+global process_list    
 process_list = []
 
 def ssh(host, arguments):
-    global remote_list
+    """Process starter using ssh through subprocess. No loadbalancing yet."""
     logger = Logger()
-    """SSH process starter. Non-loadbalancing."""
     python_path = os.path.dirname(os.path.abspath(__file__)) + "/../"
     sshexec = ["ssh"] + [host] + ["PYTHONPATH=" + python_path ]+ arguments 
     logger.debug("Exec: %s" % (' '.join(sshexec)))
+    # Execute the ssh command in a subprocess
     p = subprocess.Popen(sshexec, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process_list.append(p)
     return p
     
 def popen(host, arguments):
-    global process_list
+    """ Process starter using subprocess. No loadbalancing yet"""
     logger = Logger()
 
     if _islocal(host):
@@ -48,6 +41,16 @@ def popen(host, arguments):
         raise MPIException("This processloader can only start processes on localhost, '%s' specified." % host)
 
 def wait_for_shutdown(process_list):
+    """
+    Go through list of processes and make sure they all have terminated
+    
+    NOTE: This function does not currently make sense to me. We go through all the
+    processes in the list and remove them if they are still running (poll() = None)
+    or if they have exited correctly (poll() = 0) but not if they returned status != 0
+    (which should be error).
+    Then we sleep and poll the erroneous ones again?
+    This does not make sense, if Chewbacca lives on Endor you must acquit!
+    """
     logger = Logger()
     while process_list:
         for p in process_list:
@@ -58,7 +61,7 @@ def wait_for_shutdown(process_list):
                 logger.debug("Process was None, so it was just removed from the process list")
                 process_list.remove( p )
             elif returncode == 0:
-                logger.debug("Process exited with a status of 0. Frederik can't code for shit.. stupido")
+                logger.debug("Process exited with a status of 0.")
                 process_list.remove( p )
             else:
                 logger.debug("Process exited with status: %d" % returncode)
