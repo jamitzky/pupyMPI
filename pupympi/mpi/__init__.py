7bb302ca584bd107d3233fb014322984598b6847
hantho <fhantho@yahoo.com>
2009-08-31 21:31:29 +0200

    Less (debugging) is more

---
+++ pupympi/mpi/__init__.py
@@ -0,0 +1,231 @@
+__version__ = 0.01 # It bumps the version or else it gets the hose again!
+
+
+from optparse import OptionParser, OptionGroup
+import threading
+import sys, getopt, time
+
+from mpi.communicator import Communicator
+from mpi.logger import Logger
+from mpi.network.tcp import TCPNetwork as Network
+
+def flush_all():
+    """
+    Make sure all pipes are flushed
+    """
+    logger = Logger()
+
+    for input in (sys.stdout, sys.stderr):
+        try:
+            input.flush()
+            logger.debug("%s flushed" % input.name)
+        except ValueError():
+            logger.debug("Error in flushing %s" % input.name)
+
+class MPI(threading.Thread):
+    """
+    This is the main class containing most of the public API. Initializing 
+    the MPI system is done by creating an instance of this class. Through a
+    MPI instance a program can interact with other processes through different
+    communicators. 
+
+    NOTE: The MPI instance state is a static class variable, so creating multiple
+    instances will always yield 'the same' instance, much like a singleton design
+    pattern. 
+    """
+
+    @classmethod
+    def initialize(cls): # {{{1
+        """
+        Initializes the MPI environment. This will give each process a separate
+        rank in the MPI_COMM_WORLD communicator along with the total number of
+        processes in the communicator. Both attributes can be read just after
+        startup::
+
+            from mpi import MPI
+
+            mpi = MPI()
+            rank = mpi.MPI_COMM_WORLD.rank()
+            size = mpi.MPI_COMM_WORLD.size()
+        
+            print "Proc %d of %d started" % (rank, size)
+            
+        """
+        parser = OptionParser()
+        parser.add_option('--rank', type='int')
+        parser.add_option('--size', type='int')
+        parser.add_option('--verbosity', type='int')
+        parser.add_option('--debug', action='store_true')
+        parser.add_option('--quiet', action='store_true')
+        parser.add_option('--log-file', dest='logfile', default="mpi")
+        parser.add_option('--network-type', dest='network_type')
+        parser.add_option('--mpirun-conn-port', dest='mpi_conn_port')
+        parser.add_option('--mpirun-conn-host', dest='mpi_conn_host')
+        parser.add_option('--single-communication-thread', dest='single_communication_thread')
+
+        options, args = parser.parse_args()
+    
+        mpi = MPI()
+        mpi.shutdown_lock = threading.Lock()
+        mpi.startup(options, args)
+        mpi.start()
+        return mpi
+    # }}}1
+
+    def run(self):
+        # This can be moved out of there
+        while True:
+            """
+            FIXME:
+            I belive the conditions on this thing are inverted:
+            When invoked with the blocking argument set to false, do not block.
+            If a call without an argument would block, return false immediately;
+            otherwise, do the same thing as when called without arguments, and return true.
+            - http://docs.python.org/library/threading.html
+            ... so if we cant get the lock the acquire call returns False which the
+            not negates to true and thus the if statement is executed...
+            """
+            # Check for shutdown
+            if not self.shutdown_lock.acquire(False):
+                self.shutdown_lock.release()
+                break
+            self.shutdown_lock.release() # TODO: This should be superflous and possibly bad
+            """
+            The release() method should only be called in the locked state; it
+            changes the state to unlocked and returns immediately. If an attempt
+            is made to release an unlocked lock, a RuntimeError will be raised.
+            - http://docs.python.org/library/threading.html
+            """
+            # Update request objects
+            for comm in self.communicators:
+                comm.update()                
+            
+            # Trying to flush pipes
+            flush_all()
+
+            time.sleep(1)
+
+    def startup(self, options, args): # {{{1
+        # Initialise the logger
+        logger = Logger(options.logfile, "proc-%d" % options.rank, options.debug, options.verbosity, options.quiet)
+        # Let the communication handle start up if it need to.
+
+        logger.debug("Finished all the runtime arguments")
+
+        # Starting the network. This is probably a TCP network, but it can be 
+        # replaced pretty easily if we want to. 
+        self.network = Network(options)
+
+        # Create the initial communicator MPI_COMM_WORLD. It's initialized with 
+        # the rank of the process that holds it and size.
+        # The members are filled out after the network is initialized.
+        self.MPI_COMM_WORLD = Communicator(options.rank, options.size, self.network)
+        self.communicators = []
+        self.communicators.append( self.MPI_COMM_WORLD )
+
+        # Tell the communicator to build it's "world" from the results of the network
+        # initialization. All network types will create a variable called all_procs
+        # containing network specific information for all ranks.
+        self.MPI_COMM_WORLD.build_world( self.network.all_procs )
+
+        # Tell the network about the global MPI_COMM_WORLD, and let it start to 
+        # listen on the correcsponding network channels
+        self.network.set_mpi_world( self.MPI_COMM_WORLD )
+
+        # Change the contents of sys.argv runtime, so the user processes 
+        # can't see all the mpi specific junk parameters we start with.
+        user_options =[sys.argv[0], ] 
+        user_options.append(sys.argv[sys.argv.index("--")+1:])
+        sys.argv = user_options
+
+        # Set a static attribute on the class so we know it's initialised.
+        self.__class__._initialized = True
+        logger.debug("Set the MPI environment to initialised")
+    # }}}1
+
+    def finalize(self):
+        """
+        This method cleans up after a MPI run. Closes filehandles, 
+        logfiles and sockets. 
+
+        FIXME: Should we manually try to kill some threads?
+        """
+        # Wait for shutdown to be signalled
+        self.shutdown_lock.acquire()
+        # Shutdown the network
+        self.network.finalize()
+        Logger().debug("Network finalized")
+
+        # Trying to flush pipes
+        flush_all()
+
+    @classmethod
+    def initialized(cls):
+        """
+        Returns a boolean indicating whether the MPI environment is 
+        initialized:: 
+
+            from mpi import MPI
+
+            status = MPI.initialized()  # status will be False
+            mpi = MPI.initialize()
+
+            status = MPI.initialized()  # status will now be True
+
+        Please, if you're thinking of using this method, you might
+        be down the wrong track. Don't bend the Python like Bromer does. 
+        """
+        return getattr(cls, '_initialized', False)
+        
+    #### Communicator creation, deletion
+    def comm_create(self, arg):
+        """
+        This function creates a new communicator newcomm with communication group defined by group and a new context. No cached information propagates from comm to newcomm. The function returns MPI_COMM_NULL (None) to processes that are not in group. The call is erroneous if not all group arguments have the same value, or if group is not a subset of the group associated with comm. Note that the call is to be executed by all processes in comm, even if they do not belong to the new group. This call applies only to intra-communicators. 
+        
+        http://www.mpi-forum.org/docs/mpi-11-html/node102.html
+        """
+        Logger().warn("Non-Implemented method 'comm_create' called.")
+
+    def comm_free(self, existing_communicator):
+        """
+        This collective operation marks the communication object for deallocation. The handle is set to MPI_COMM_NULL. Any pending operations that use this communicator will complete normally; the object is actually deallocated only if there are no other active references to it. This call applies to intra- and inter-communicators. The delete callback functions for all cached attributes (see section Caching ) are called in arbitrary order.
+    
+        http://www.mpi-forum.org/docs/mpi-11-html/node103.html#Node103
+        """
+        Logger().warn("Non-Implemented method 'comm_free' called.")
+
+
+    def comm_split(self, existing_communicator, color = None, key = None):
+        """
+        FIXME
+        """
+        Logger().warn("Non-Implemented method 'comm_split' called.")
+
+    def comm_dup(self, todo_args_missing):
+        """
+        FIXME
+        """
+        Logger().warn("Non-Implemented method 'comm_dup' called.")
+        
+    def comm_compare(self, communicator1, communicator2):
+        """
+        MPI_IDENT results if and only if comm1 and comm2 are handles for the same object (identical groups and same contexts). MPI_CONGRUENT results if the underlying groups are identical in constituents and rank order; these communicators differ only by context. MPI_SIMILAR results if the group members of both communicators are the same but the rank order differs. MPI_UNEQUAL results otherwise. 
+        
+        http://www.mpi-forum.org/docs/mpi-11-html/node101.html#Node101
+        """
+        Logger().warn("Non-Implemented method 'comm_compare' called.")
+        
+    def get_count(self, arg):
+        # FIXME
+        pass
+        
+    def get_elements(self, arg):
+        # FIXME
+        pass
+    def get_processor_name(self, arg):
+        # FIXME
+        pass
+        
+    def get_version(self):
+        # FIXME (doc)
+        return __version__
---
+++ pupympi/mpi/network/__init__.py
@@ -0,0 +1,84 @@
+import threading
+from threading import Thread
+from mpi.logger import Logger
+
+class AbstractNetwork(object):
+
+    def _not_impl(self):
+        raise NotImplementedError("Don't use the Network class directly. Please use a inherited class")
+
+    isend = _not_impl
+    send = _not_impl
+    recv = _not_impl
+    irecv = _not_impl
+    initialize = _not_impl
+    finalize = _not_impl
+    start_job = _not_impl
+
+    def __init__(self, CommunicationHandler, options):
+        Logger().debug("Starting generic network")
+
+        # Defining some "queues", just simple lists for now
+        self.incomming = []
+        self.outgoing = []
+        self.options = options
+        rank = options.rank
+
+        if options.single_communication_thread:
+            self.t_in = CommunicationHandler(rank, self.incomming, self.outgoing)
+            self.t_out = self.t_in
+        else:
+            self.t_in = CommunicationHandler(rank, self.incomming, None)
+            self.t_out = CommunicationHandler(rank, None, self.outgoing)
+            self.t_out.start()
+
+        self.t_in.start()
+
+    def get_received_data(self, participant, tag, communicator):
+        return self.t_in.get_received_data(participant, tag, communicator)
+
+    def finalize(self):
+        """
+        Forwarding the finalize call to the threads. Look at the 
+        CommunicationHandler.finalize for a deeper description of
+        the shutdown procedure. 
+        """
+        self.t_in.finalize()
+        if not self.options.single_communication_thread:
+            self.t_out.finalize()
+
+class AbstractCommunicationHandler(Thread):
+    def __init__(self, rank, incomming, outgoing):
+        Thread.__init__(self)
+        self.incomming = incomming 
+        self.outgoing = outgoing
+        self.rank = rank
+
+        self.shutdown_lock = threading.Lock()
+        self.shutdown_lock.acquire()
+
+    def add_in_job(self, job):
+        self.incomming.append(job)
+
+    def add_out_job(self, job):
+        self.outgoing.append(job)
+
+    def shutdown_ready(self):
+        acquired = self.shutdown_lock.acquire(False)
+        if acquired:
+            self.shutdown_lock.release()
+
+        #Logger().debug("CommunicationHandler asked for shutdown status: %s" % acquired)
+        return acquired
+
+    def finalize(self):
+        """
+        When each thread starts a shutdown lock is acquired. As long as this
+        lock is taken the thread can't shut down. So when finalized is called
+        we release the lock. 
+
+        This mean that the shutdown_ready() funtion can acquire it. This is used
+        in the main run loop and allows the thread to break out of the while True
+        loop. 
+        """
+        self.shutdown_lock.release()
