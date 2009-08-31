8abdf43fc1a29ec2717b64e31c15b45dc58dc9cd
hantho <fhantho@yahoo.com>
2009-08-31 21:59:05 +0200

    test programs combining isend with recv and send with irecv

--- pupympi/tests/test_simple_send.py
+++ pupympi/tests/test_simple_send.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python2.6
 
-# Is this test program used anymore???
+# Simple pupympi program to test basic immediate send to blocking recieve
+# This test is meant to be run with only two processes
 
-# Simple pupympi program to test basic communication between to processes
+# first rank 0 isends timestamp to rank 1 who is a very slow reciever so rank 0 should quit early
 
 import time
-from mpi import tcp, MPI
-
-MY_TAG = 12
+from mpi import MPI
 
 mpi = MPI.initialize()
 
-rank = mpi.rank()
-size = mpi.size()
+rank = mpi.MPI_COMM_WORLD.rank()
+size = mpi.MPI_COMM_WORLD.size()
+
+
+neighbour = (rank + 1) % size # Communicate with own rank + 1
+content = "This message was Isent"
+
+DUMMY_TAG = 1
+
 
 if rank == 0:
-    mpi.send(1, "My contents", MY_TAG)
-else:
-    data = mpi.recv(1, MY_TAG)
-    print "Got data: ", data
+    # Send
+    print "Rank: %d sending to %d" % (rank,neighbour)
+    request = mpi.MPI_COMM_WORLD.isend(neighbour,content,DUMMY_TAG)
+    print "Rank: %d DONE sending to %d" % (rank,neighbour)
+    request.wait()
+    
+    print "Rank: %d ALL DONE" % (rank)
+
+    
+else: # rank == 1
+    # Waaaaait for it...
+    time.sleep(4)
+    
+    # Recieve
+    print "YAWN, rank: %d recieving from %d" % (rank,neighbour)
+    recieved = mpi.MPI_COMM_WORLD.recv(neighbour,DUMMY_TAG)    
+    #request = mpi.MPI_COMM_WORLD.irecv(neighbour,DUMMY_TAG)    
+    #recieved = request.wait()
+    print "Rank: %d RECIEVED %s" % (rank,recieved)
+
+
+print "Sending/recieving done rank %d of %d" % (rank, size)
 
 # Close the sockets down nicely
 mpi.finalize()
