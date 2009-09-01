d302fcad825cf0d39c4f8f9c7c90742c7fb18df5
Rune Bromer <rune.bromer@coniuro.dk>
2009-09-01 16:18:11 +0200

    Implemented generic callback handler. Just pass anything to it and your done

---
+++ pupympi/tests/TEST_initialized.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2.6
+
+import time
+from mpi import MPI
+
+assert not MPI.initialized(), "The mpi environment was initialized before starting mpi.. wrong"
+
+mpi = MPI.initialize()
+
+assert MPI.initialized(), "The mpi environment was not initialized after starting mpi.. wrong"
+
+# Close the sockets down nicely
+mpi.finalize()
+
