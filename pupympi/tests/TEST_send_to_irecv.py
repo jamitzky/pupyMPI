c203d27af53c51811e73e953c4eb3eb411d94709
Rune Bromer <rune.bromer@coniuro.dk>
2009-09-17 19:11:32 +0200

    This fixes all the fixable tests cases that depended on us running the tests with -c 2.
    fixes #29

--- pupympi/tests/TEST_send_to_irecv.py
+++ pupympi/tests/TEST_send_to_irecv.py
@@ -13,23 +13,20 @@ mpi = MPI()
 rank = mpi.MPI_COMM_WORLD.rank()
 size = mpi.MPI_COMM_WORLD.size()
 
-
-neighbour = (rank + 1) % size # Communicate with own rank + 1
 content = "This message was sent"
-
 DUMMY_TAG = 1
-
 
 if rank == 0:
     # Send
+    neighbour = 1
     print "Rank: %d sending to %d" % (rank,neighbour)
     request = mpi.MPI_COMM_WORLD.send(neighbour,content,DUMMY_TAG)    
     print "Rank: %d ALL DONE" % (rank)
 
     
-else: # rank == 1
+elif rank == 1: # rank == 1
     # Waaaaait for it...
-    time.sleep(4)
+    neighbour = 0
     
     # Recieve
     print "YAWN, rank: %d recieving from %d" % (rank,neighbour)
@@ -37,7 +34,8 @@ else: # rank == 1
     request = mpi.MPI_COMM_WORLD.irecv(neighbour,DUMMY_TAG)    
     recieved = request.wait()
     print "Rank: %d RECIEVED %s" % (rank,recieved)
-
+else:
+    print "I'm rank %d and I'm not doing anything in this test" % rank
 
 print "Sending/recieving done rank %d of %d" % (rank, size)
 
