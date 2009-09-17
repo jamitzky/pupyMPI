61537ebbc8a69c494811beb0736caaa42fb9ffd0
Rune Bromer <rune.bromer@coniuro.dk>
2009-09-17 19:11:32 +0200

    This fixes all the fixable tests cases that depended on us running the tests with -c 2.
    fixes #29

--- pupympi/tests/TEST_send_to_irecv2.py
+++ pupympi/tests/TEST_send_to_irecv2.py
@@ -15,25 +15,22 @@ mpi = MPI()
 rank = mpi.MPI_COMM_WORLD.rank()
 size = mpi.MPI_COMM_WORLD.size()
 
-
-neighbour = (rank + 1) % size # Communicate with own rank + 1
 content1 = "This is message 1"
 content2 = "This is message 2"
 
 DUMMY_TAG = 1
 ANOTHER_TAG = 2
 
-
 if rank == 0:
+    neighbour = 1
     # Send
     print "Rank: %d sending to %d" % (rank,neighbour)
     request = mpi.MPI_COMM_WORLD.send(neighbour,content2,DUMMY_TAG)    
     request = mpi.MPI_COMM_WORLD.send(neighbour,content1,ANOTHER_TAG)    
     print "Rank: %d ALL DONE" % (rank)
 
-    
-else: # rank == 1
-    
+elif rank == 1:
+    neighbour = 0
     # Recieve
     print "YAWN, rank: %d recieving from %d" % (rank,neighbour)
     request1 = mpi.MPI_COMM_WORLD.irecv(neighbour,ANOTHER_TAG)    
@@ -42,6 +39,8 @@ else: # rank == 1
     request2 = mpi.MPI_COMM_WORLD.irecv(neighbour,DUMMY_TAG)    
     recieved = request2.wait()
     print "Rank: %d RECIEVED %s for request2" % (rank,recieved)
+else: 
+    print "I'm rank %d and I'm not doing anything in this test" % rank
 
 
 
--- pupympi/tests/TEST_simple_immediate_communication_1.py
+++ pupympi/tests/TEST_simple_immediate_communication_1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.6
 
 # Simple pupympi program to test basic immediate communication
-
 # first rank 0 isends timestamp to rank 1 who is a very slow reciever so rank 0 should quit early
 
 import time
@@ -12,13 +11,11 @@ mpi = MPI()
 rank = mpi.MPI_COMM_WORLD.rank()
 size = mpi.MPI_COMM_WORLD.size()
 
-
-neighbour = (rank + 1) % size # Send to own rank + 1
 content = "Message from rank %d" % (rank)
-
 DUMMY_TAG = 1
 
 if rank == 0:
+    neighbour = 1
     # Send
     print "Rank: %d sending to %d" % (rank,neighbour)
     request = mpi.MPI_COMM_WORLD.isend(neighbour,content,DUMMY_TAG)
@@ -27,17 +24,16 @@ if rank == 0:
     print "Rank: %d DONE" % (rank)
 
     
-else: # rank == 1
-    # TOFIX: This sleep actually deadlocks the program - it shouldn't
-    # Waaaaait for it...
-    time.sleep(4)
+elif rank == 1: 
+    neighbour = 0
     
     # Recieve
     print "Yawn, rank: %d recieving from %d" % (rank,neighbour)
     request = mpi.MPI_COMM_WORLD.irecv(neighbour,DUMMY_TAG)    
     recieved = request.wait()
     print "Rank: %d recieved %s" % (rank,recieved)
-
+else:
+    print "I'm rank %d and I'm not doing anything in this test" % rank
 
 print "Sending/recieving done rank %d of %d after %d seconds sleep" % (rank, size, size-rank)
 
