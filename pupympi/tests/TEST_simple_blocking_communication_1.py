59171b17a09c95eda16489ad3ea78bd67ac43f23
bromer <bromer@tyr.diku.dk>
2009-09-17 14:50:45 +0200

    The test was bound to handling a size() of 2. It can now handle size > 1. Rocket science

--- pupympi/tests/TEST_simple_blocking_communication_1.py
+++ pupympi/tests/TEST_simple_blocking_communication_1.py
@@ -13,14 +13,17 @@ mpi = MPI()
 rank = mpi.MPI_COMM_WORLD.rank()
 size = mpi.MPI_COMM_WORLD.size()
 
+if size < 2:
+    print "WHAT ARE YOU DOING?"
 
-neighbour = (rank + 1) % size # Send to own rank + 1
+
 content = "HELLO.. This is my bloddy message from rank %d" % (rank)
 
 DUMMY_TAG = 1
 
 if rank == 0:
     # Send
+    neighbour = 1
     print "Rank: %d sending to %d" % (rank,neighbour)
     mpi.MPI_COMM_WORLD.send(neighbour,content, DUMMY_TAG)
 
@@ -28,9 +31,10 @@ if rank == 0:
     print "Rank: %d recieving from %d" % (rank,neighbour)
     recieved = mpi.MPI_COMM_WORLD.recv(neighbour, DUMMY_TAG)    
     print "Rank: %d recieved '%s'" % (rank,recieved)
-    
-else: # rank == 1
+
+elif rank == 1:
     # Recieve
+    neighbour = 0
     print "Rank: %d recieving from %d" % (rank,neighbour)
     recieved = mpi.MPI_COMM_WORLD.recv(neighbour, DUMMY_TAG)    
     print "Rank: %d recieved '%s'" % (rank,recieved)
@@ -38,6 +42,8 @@ else: # rank == 1
     # Send
     print "Rank: %d sending to %d" % (rank,neighbour)
     mpi.MPI_COMM_WORLD.send(neighbour,content, DUMMY_TAG)
+else: # rank == 1
+    print "Im doing nothing"
 
 print "Sending/recieving done rank %d of %d after %d seconds sleep" % (rank, size, size-rank)
 
