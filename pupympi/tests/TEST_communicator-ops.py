681aa3260fabc15ee412595d5a0b98062f8f9536
Jan Wiberg <jan@lykke.org>
2009-09-23 11:21:09 +0200

    local communicator creation is up

--- pupympi/tests/TEST_communicator-ops.py
+++ pupympi/tests/TEST_communicator-ops.py
@@ -11,6 +11,20 @@ newgroup = mpi.MPI_COMM_WORLD.group()
 
 newcomm_full = mpi.MPI_COMM_WORLD.comm_create(newgroup)
 assert newcomm_full is not None
+print "Communicator ID: %s" % newcomm_full.id
+
+newcomm_full2 = mpi.MPI_COMM_WORLD.comm_create(newgroup)
+assert newcomm_full2 is not None
+print "Communicator ID: %s" % newcomm_full2.id
+
+newcomm_full3 = newcomm_full.comm_create(newgroup)
+assert newcomm_full3 is not None
+print "Communicator ID: %s" % newcomm_full3.id
+
+newcomm_full4 = newcomm_full2.comm_create(newgroup)
+assert newcomm_full4 is not None
+print "Communicator ID: %s" % newcomm_full4.id
+
 
 # Close the sockets down nicely
 mpi.finalize()
