5539fd3ba673406984406ee172718e312bc008b6
Jan Wiberg <jan@lykke.org>
2009-09-23 17:42:19 +0200

    I forget...

--- pupympi/mpi/constants.py
+++ pupympi/mpi/constants.py
@@ -1,7 +1,7 @@
 
 # Predefined tags
 MPI_TAG_ANY = -1 # public
-TAG_BCAST   = -128 # internal, this and below
+TAG_BCAST   = -128 # internal, this and below. Note they do not have to be powers of two, but Rune is being silly.
 TAG_EMPTY   = -256
 TAG_BARRIER = -512
 TAG_COMM_CREATE = -1024
