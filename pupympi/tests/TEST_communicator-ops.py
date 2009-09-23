c4a213e987ccc62da5973cedb567059480220194
Jan Wiberg <jan@lykke.org>
2009-09-23 14:34:13 +0200

    implemented attribute callback functions and comm_dup, fixed bug in communicator.__repr__

---
+++ pupympi/tests/TEST_communicator-ops.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.6
+
+# Simple pupympi program to test all communicator ops
+
+from mpi import MPI
+from sys import stderr
+
+mpi = MPI()
+
+newgroup = mpi.MPI_COMM_WORLD.group()
+
+newcomm_full = mpi.MPI_COMM_WORLD.comm_create(newgroup)
+assert newcomm_full is not None
+print "Communicator ID: %s. Created from mpi comm world." % newcomm_full.id
+
+newcomm_full2 = mpi.MPI_COMM_WORLD.comm_create(newgroup)
+assert newcomm_full2 is not None
+print "Communicator ID: %s. Also created from mpi comm world." % newcomm_full2.id
+
+newcomm_full3 = newcomm_full.comm_create(newgroup)
+assert newcomm_full3 is not None
+print "Communicator ID: %s. Split from the first mcw clone" % newcomm_full3.id
+
+newcomm_full4 = newcomm_full2.comm_create(newgroup)
+assert newcomm_full4 is not None
+print "Communicator ID: %s. Split from the second mcw clone" % newcomm_full4.id
+
+newcomm_full4.comm_free() 
+
+cloned_comm_of_mcw = mpi.MPI_COMM_WORLD.comm_dup()
+assert cloned_comm_of_mcw is not None
+assert cloned_comm_of_mcw.group() is newgroup
+print "Cloned mcw: %s" % cloned_comm_of_mcw
+
+
+# Close the sockets down nicely
+mpi.finalize()
