72faca773b69b1b304c5c9a9978f24c7a05e616a
Rune Bromer <rune.bromer@coniuro.dk>
2009-09-23 11:33:35 +0200

    Made the test smart enough to assert the wanted result

--- pupympi/tests/TEST_allreduce.py
+++ pupympi/tests/TEST_allreduce.py
@@ -1,12 +1,21 @@
 from mpi import MPI
 from mpi.operations import prod
 
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+    
 mpi = MPI()
 
 # We start n processes, and try to calculate n!
 rank = mpi.MPI_COMM_WORLD.rank()
-fact = mpi.MPI_COMM_WORLD.allreduce(rank+1, prod)
+size = mpi.MPI_COMM_WORLD.size()
+dist_fact = mpi.MPI_COMM_WORLD.allreduce(rank+1, prod)
 
-print "I'm rank %d and I also got the result %d. So cool" % (rank, fact)
+print "I'm rank %d and I also got the result %d. So cool" % (rank, dist_fact)
+
+assert fact(size) == dist_fact
 
 mpi.finalize()
