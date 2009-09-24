03602dcca06ed4c1214c194d95925e7e6193de05
Jan Wiberg <jan@lykke.org>
2009-09-24 14:25:59 +0200

    Added wtime() and wtick() as very basic operations

--- pupympi/mpi/constants.py
+++ pupympi/mpi/constants.py
@@ -1,6 +1,8 @@
 
 # Predefined tags
-MPI_TAG_ANY = -1 # public
+MPI_TAG_ANY = -1 # public, this and below
+MPI_SOURCE_ANY = -1
+
 TAG_BCAST   = -128 # internal, this and below. Note they do not have to be powers of two, but Rune is being silly.
 TAG_EMPTY   = -256
 TAG_BARRIER = -512
