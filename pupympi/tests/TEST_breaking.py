b5e3b5c9fe19916fb42c830da0730022eb9b3d28
hantho <fhantho@yahoo.com>
2009-10-01 14:58:34 +0200

    Modified cyclic2 to accept np>2. The communicate in lockstep with "neighbours". Some ssh_exchange failure error message seen when starting more than 15 processes.

---
+++ pupympi/tests/TEST_breaking.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.6
+
+# Simple pupympi program to test if horribly broken scripts manage to return their stderr
+
+from mpi import MPI
+from sys import stderr
+from threading import Thread, Condition, activeCount, currentThread 
+
+#print "Threads before initialize %s" % activeCount()
+mpi = MPI()
+#print "Threads after initialize %s" % activeCount()
+
+raise Exception("Forced error from rank %s" % mpi.MPI_COMM_WORLD.rank())
+
+# Close the sockets down nicely
+mpi.finalize()
