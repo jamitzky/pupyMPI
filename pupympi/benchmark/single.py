d61dc97dde7afd137ed8807c06a9ac9c49d1a480
Jan Wiberg <jan@lykke.org>
2009-10-02 20:50:03 +0200

    added single.test_pingping, this triggers 'Exception: EXTRA RELEASE' almost immediately

--- pupympi/benchmark/single.py
+++ pupympi/benchmark/single.py
@@ -29,39 +29,20 @@ def setup_single_test(size, single_test, iteration_schedule = None):
     return time
     
 
-def exif_PingPing(size, iteration_schedule = None):
-    return 0 # FIXME
-    
-    (s_tag, r_tag) = ci.get_tags_single()
-    (source, dest) = ci.get_dest_single()
-    data = common.gen_testset(size)
-    max_iterations = ci.get_iter_single(iteration_schedule, size)
-
-    ci.barrier()
-
-    t1 = ci.clock_function()
-
-    for r in xrange(iteration_schedule[size] if iteration_schedule is not None else 1000):
+def test_PingPing(s_tag, r_tag, source, dest, data, max_iterations):
+    for r in max_iterations:
+        print "pingping: rank %s iteration %s, source %s, dest %s, datalen %s" % (ci.rank, r, source, dest, len(data))
         # FIXME error handling for all statements
-        ci.communicator.isend(dest, data)
+        request = ci.communicator.isend(dest, data)
         recv_data = ci.communicator.recv(source)
+        request.wait()
         # FIXME ierr = MPI_Wait(&request, &stat);
   	    
         # TODO: check for defects
-
-          # CHK_DIFF("PingPing",ci, (char*)ci->r_buffer+i%ITERATIONS->r_cache_iter*ITERATIONS->r_offs,
-          #           0, size, size, asize,
-          #           put, 0, ITERATIONS->n_sample, i,
-          #           dest, &defect);
-
-    t2 = ci.communicator.Wtime()
-    time = (t2 - t1)/1000 # FIXME iter_sched
-        
-    return time
     
 def test_PingPong(s_tag, r_tag, source, dest, data, max_iterations):
     for r in max_iterations:
-        print "rank %s iteration %s, source %s, dest %s, datalen %s" % (ci.rank, r, source, dest, len(data))
+        print "pingpong: rank %s iteration %s, source %s, dest %s, datalen %s" % (ci.rank, r, source, dest, len(data))
         if ci.rank == ci.pair0: 
             ci.communicator.send(dest, data, s_tag)
             recv = ci.communicator.recv(source, r_tag)
