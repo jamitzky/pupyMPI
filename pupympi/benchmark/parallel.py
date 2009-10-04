ba353a482fdc22c353cc3e5f6a5683f339922d8e
Jan Wiberg <jan@lykke.org>
2009-10-04 14:26:43 +0200

    change: communicators now register self with mpi context

--- pupympi/benchmark/parallel.py
+++ pupympi/benchmark/parallel.py
@@ -9,6 +9,27 @@ Created by Jan Wiberg on 2009-08-13.
 import sys
 import os
 
+def do_test(size, parallel_test, iteration_schedule = None):
+    """
+    Sets up environment, times and runs test
+    """
+    (s_tag, r_tag) = ci.get_tags_single()
+    (source, dest) = ci.get_dest_single() # source for purposes of recv, rank-relative
+    data = common.gen_testset(size)
+    max_iterations = ci.get_iter_single(iteration_schedule, size)
+
+    ci.synchronize_processes()
+
+    t1 = ci.clock_function()
+    
+    # do magic
+    parallel_test(s_tag, r_tag, source, dest, data, max_iterations)
+    
+    t2 = ci.clock_function()
+    time = (t2 - t1)/len(max_iterations) 
+
+    return time
+    
 def Sendrecv():
     pass
     
