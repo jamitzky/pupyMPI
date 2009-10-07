60b35fefeb703281eb2bca77dca472dc1d85d193
Jan Wiberg <jan@lykke.org>
2009-10-07 13:08:26 +0200

    reordered functions a bit

--- pupympi/benchmark/common.py
+++ pupympi/benchmark/common.py
@@ -3,6 +3,8 @@
 """
 common.py
 
+this file is "deprecated". Its contents should be moved into comm_info.py
+
 Created by Jan Wiberg on 2009-08-13.
 Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 """
