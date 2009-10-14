7a9e978d6c49dd17349fda4935fe06d70377b013
hantho <fhantho@yahoo.com>
2009-10-14 18:41:40 +0200

    moved network code to several files

---
+++ pupympi/mpi/network/utils.py
@@ -0,0 +1,35 @@
+import socket
+
+def get_socket(min=10000, max=30000):
+    """
+    A simple helper method for creating a socket,
+    binding it to a random free port within the specified range. 
+    """
+    logger = Logger()
+    used = []
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    
+    hostname = socket.gethostname()
+    port_no = None
+
+    #logger.debug("get_socket: Starting loop with hostname %s" % hostname)
+
+    while True:
+        port_no = random.randint(min, max) 
+        if port_no in used:
+            logger.debug("get_socket: Port %d is already in use %d" % port_no)
+            continue
+
+        try:
+            #logger.debug("get_socket: Trying to bind on port %d" % port_no)
+            sock.bind( (hostname, port_no) )
+            break
+        except socket.error, e:
+            logger.debug("get_socket: Permission error on port %d" % port_no)
+            used.append( port_no ) # Mark socket as used (or no good or whatever)
+            raise e
+        
+    #logger.debug("get_socket: Bound socket on port %d" % port_no)
+    return sock, hostname, port_no
