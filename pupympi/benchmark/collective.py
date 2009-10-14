6407b4c2f00cd68ab1ce5fc4c286bf705558f9e5
hantho <fhantho@yahoo.com>
2009-10-14 18:16:38 +0200

    Automated merge with ssh://hg@bitbucket.org/bromer/python-mpi/

--- pupympi/benchmark/collective.py
+++ pupympi/benchmark/collective.py
@@ -55,7 +55,7 @@ def test_Allgather(size, iteration_schedule = None):
         """docstring for Allgather"""
         for r in max_iterations:
             # TODO check if allgather verifies that the jth block size is modulo size 
-            ci.communicator.allgather(data[ci.rank:ci.rank+size], size, size) # FIXME allgatherv signature likely to change 
+            ci.communicator.allgather(data[ci.rank:ci.rank+size], size, size) # FIXME allgather signature may change 
 
             # FIXME defect detection and error handling
 
@@ -75,34 +75,148 @@ def test_Allgather(size, iteration_schedule = None):
     return time
 
     
-def Allgatherv():
-    pass
+def test_Allgatherv(size, iteration_schedule = None):
+    def Allgatherv(data, datalen, max_iterations):
+        """docstring for Allgather"""
+        for r in max_iterations:
+            # TODO check if allgather verifies that the jth block size is modulo size 
+            ci.communicator.allgatherv(data[ci.rank:ci.rank+size], size, ci.rdispl) # FIXME allgatherv signature may change 
+            # ierr = MPI_Allgatherv((char*)c_info->s_buffer+i%ITERATIONS->s_cache_iter*ITERATIONS->s_offs,
+            #                       s_num,c_info->s_data_type,
+            #                       (char*)c_info->r_buffer+i%ITERATIONS->r_cache_iter*ITERATIONS->r_offs,
+            #                       c_info->reccnt,c_info->rdispl,
+            #                       c_info->r_data_type,
+            #                       c_info->communicator);
+
+            # FIXME defect detection and error handling
+
+    # end of test
+    data = common.gen_testset(size)*ci.num_procs
+    max_iterations = ci.get_iter_single(iteration_schedule, size)
+    ci.rdispl = 1 # FIXME not necessarily best
+    ci.synchronize_processes()
+
+    t1 = ci.clock_function()
     
-def Alltoall():
-    pass
+    # do magic
+    Allgatherv(data, size, max_iterations)
+
+    t2 = ci.clock_function()
+    time = (t2 - t1)/len(max_iterations) 
+
+    return time
     
-def Alltoallv():
+def test_Alltoall(size, iteration_schedule = None):
+    def Alltoall(data, datalen, max_iterations):
+        """docstring for Alltoall"""
+        for r in max_iterations:
+            ci.communicator.alltoall(data[ci.rank:ci.rank+size], ci.sndcnt, data, ci.reccnt)
+                  #             ierr = MPI_Alltoall((char*)c_info->s_buffer+i%ITERATIONS->s_cache_iter*ITERATIONS->s_offs,
+                  #                                 s_num,c_info->s_data_type,
+                  # (char*)c_info->r_buffer+i%ITERATIONS->r_cache_iter*ITERATIONS->r_offs,
+                  #                                 r_num,c_info->r_data_type,
+                  # c_info->communicator);
+            # FIXME defect detection and error handling
+
+    # end of test
+    data = common.gen_testset(size)*ci.num_procs
+    max_iterations = ci.get_iter_single(iteration_schedule, size)
+    ci.rdispl = 1 # FIXME not necessarily best
+    ci.synchronize_processes()
+
+    t1 = ci.clock_function()
+    
+    # do magic
+    Allgatherv(data, size, max_iterations)
+
+    t2 = ci.clock_function()
+    time = (t2 - t1)/len(max_iterations) 
+
+    return time 
+       
+def test_Alltoallv(size, iteration_schedule = None):
+    def Alltoallv(data, datalen, max_iterations):
+        pass
+    # end of test
     pass
 
-def Scatter():
+def test_Scatter(size, iteration_schedule = None):
+    def Scatter(data, datalen, max_iterations):
+        pass
+    # end of test
     pass
 
-def Scatterv():
+def test_Scatterv(size, iteration_schedule = None):
+    def Scatterv(data, datalen, max_iterations):
+        pass
+    # end of test
     pass
 
-def Gather():
+def test_Gather(size, iteration_schedule = None):
+    def Gather(data, datalen, max_iterations):
+        pass
+    # end of test
     pass
 
-def Gatherv():
+def test_Gatherv(size, iteration_schedule = None):
+    def Gatherv(data, datalen, max_iterations):
+        pass
+    # end of test
     pass
 
-def Reduce():
+def test_Reduce(size, iteration_schedule = None):
+    def Reduce(data, datalen, max_iterations):
+        """docstring for Reduce"""
+        for r in max_iterations:
+            pass
+            
+                #           ierr = MPI_Reduce((char*)c_info->s_buffer+i%ITERATIONS->s_cache_iter*ITERATIONS->s_offs,
+                #                             (char*)c_info->r_buffer+i%ITERATIONS->r_cache_iter*ITERATIONS->r_offs,
+                #                             s_num,
+                # c_info->red_data_type,c_info->op_type,
+                # i1,c_info->communicator);
+                #           MPI_ERRHAND(ierr);
+                # 
+                #             #ifdef CHECK
+                #              if( c_info->rank == i1 )
+                #              {
+                #                   CHK_DIFF("Reduce",c_info, (char*)c_info->r_buffer+i%ITERATIONS->r_cache_iter*ITERATIONS->r_offs, 0,
+                #                            size, size, asize, 
+                #                            put, 0, ITERATIONS->n_sample, i,
+                #                            -1, &defect);
+                #              }
+                #             #endif
+                #         /*  CHANGE THE ROOT NODE */
+                #         i1=(++i1)%c_info->num_procs;
+    # end of test
+
+    # end of test
+    data = common.gen_testset(size)*ci.num_procs
+    max_iterations = ci.get_iter_single(iteration_schedule, size)
+    ci.rdispl = 1 # FIXME not necessarily best
+    ci.synchronize_processes()
+
+    t1 = ci.clock_function()
+    
+    # do magic
+    Reduce(data, size, max_iterations)
+
+    t2 = ci.clock_function()
+    time = (t2 - t1)/len(max_iterations) 
+
+    return time    
+
+
+def test_Reduce_scatter(size, iteration_schedule = None):
+    def Reduce_scatter(data, datalen, max_iterations):
+        pass
+    # end of test
     pass
 
-def Reduce_scatter():
-    pass
-
-def Allreduce():
+def test_Allreduce(size, iteration_schedule = None):
+    def Allreduce(data, datalen, max_iterations):
+        pass
+    # end of test
     pass
 
 def test_Barrier(size, iteration_schedule = None):
