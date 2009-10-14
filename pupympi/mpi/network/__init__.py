2e9a4252508be6f6d02cf92130facbade52b4bce
hantho <fhantho@yahoo.com>
2009-10-14 19:38:34 +0200

    Network getting better

--- pupympi/mpi/network/__init__.py
+++ pupympi/mpi/network/__init__.py
@@ -6,8 +6,10 @@ from mpi.network.socketpool import SocketPool
 
 class Network(object):
     def __init__(self, options):
+        self.socket_pool = SocketPool(constants.SOCKET_POOL_SIZE)
+
         self.options = options
-        self.t_in = CommunicationHandler(rank)
+        self.t_in = CommunicationHandler(self, rank, self.socket_pool)
         self.t_in.name = "t_in"
         self.t_in.daemon = True
         self.t_in.start()
@@ -15,12 +17,10 @@ class Network(object):
         if options.single_communication_thread:
             self.t_out = self.t_in
         else:
-            self.t_out = CommunicationHandler(rank)
+            self.t_out = CommunicationHandler(self, rank, self.socket_pool)
             self.t_out.daemon = True
             self.t_out.name = "t_out"
             self.t_out.start()
-
-        self.socket_pool = SocketPool(constants.SOCKET_POOL_SIZE)
         
         (socket, hostname, port_no) = get_socket()
         self.port = port_no
@@ -82,9 +82,49 @@ class Network(object):
 
 class CommunicationHandler(threading.Thread):
     """
+    This is a single thread doing both in and out or there are two threaded instances one for each
     """
-    # Add two TCP specific lists. Read and write sockets
-    self.sockets_in = []
-    self.sockets_out = []
+    def __init__(self,network, rank, socket_pool):
+        # Store all procs in the network for lookup
+        self.all_procs = network.all_procs
+        
+        # Add two TCP specific lists. Read and write sockets
+        self.sockets_in = []
+        self.sockets_out = []
+        
+        # Structure for finding request objects from a socket connection.
+        # a dict mapping a socket key to a list of requests on that socket
+        self.socket_to_request = {}
+        
+        self.rank = rank
+        self.socket_pool = socket_pool
     
-    
+    def add_request(self, request):
+        if request.request_type in ('bcast_send', 'send'):
+            
+            # Find the global rank of other party
+            global_rank = request.communicator.group().members[request[participant]]['global_rank']
+            
+            # Find a socket and port of other party           
+            host = self.all_procs[global_rank]['host']
+            port = self.all_procs[global_rank]['port']
+
+            socket = self.socket_pool.get_socket(global_rank, host, port)
+            
+            # Add the socket and request to the internal system
+            if socket in self.socket_to_request:
+                self.socket_to_request[socket].append(request) # socket already exists just add another request to the list
+            else:
+                self.socket_to_request[socket] = [ request ] # new socket, add the request in a singleton list
+            
+    def remove_request(request):
+        """
+        For now we try to remove the request from all lists not just the right socket's list
+        This is handled by except, but could be done nicer.    
+        """
+        for socket in self.socket_to_request:
+            try:
+                self.socket_to_request[socket].remove(request)
+                break # only one socket is the right one, we found it
+            except ValueError:
+                pass # The request was just not in the list (aka. wrong socket try next one)
