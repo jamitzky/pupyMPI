d20002213edc09c7354f24ae5bf03c73e17c98b7
hantho <fhantho@yahoo.com>
2009-10-17 12:46:52 +0200

    reminder that bcast needs >2 procs

--- pupympi/tests/TEST_bcast.py
+++ pupympi/tests/TEST_bcast.py
@@ -1,6 +1,9 @@
 from mpi import MPI
 BCAST_MESSAGE = "Test message"
 mpi = MPI()
+size = mpi.MPI_COMM_WORLD.size()
+assert size > 2
+
 if mpi.MPI_COMM_WORLD.rank() == 3:
     mpi.MPI_COMM_WORLD.bcast(3, BCAST_MESSAGE)
 else:
