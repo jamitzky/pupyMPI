5fa7822cb05199d160473a267ef4da7df596d028
Rune Bromer <rune.bromer@coniuro.dk>
2009-10-25 19:03:35 +0100

    Made the bcast root a variable so it's easier to test with different roots.

--- pupympi/tests/TEST_bcast.py
+++ pupympi/tests/TEST_bcast.py
@@ -11,15 +11,17 @@ BCAST_MESSAGE = "Test message"
 mpi = MPI()
 init_time = time.time()
 
+BCAST_ROOT = 0
+
 print "Init time: %f" % (init_time-start_time)
 
 size = mpi.MPI_COMM_WORLD.size()
 assert size > 2
 
-if mpi.MPI_COMM_WORLD.rank() == 3:
-    mpi.MPI_COMM_WORLD.bcast(3, BCAST_MESSAGE)
+if mpi.MPI_COMM_WORLD.rank() == BCAST_ROOT:
+    mpi.MPI_COMM_WORLD.bcast(BCAST_ROOT, BCAST_MESSAGE)
 else:
-    message = mpi.MPI_COMM_WORLD.bcast(3)
+    message = mpi.MPI_COMM_WORLD.bcast(BCAST_ROOT)
     print message
     assert message == BCAST_MESSAGE
 
