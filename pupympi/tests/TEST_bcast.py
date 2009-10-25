fea02b274758b0408ba026b0a4f258aa17bbcafe
Rune Bromer <rune.bromer@coniuro.dk>
2009-10-25 19:08:00 +0100

    Made the bcast test a lot nicer to read

--- pupympi/tests/TEST_bcast.py
+++ pupympi/tests/TEST_bcast.py
@@ -3,37 +3,22 @@
 # meta-expectedresult: 0
 # meta-minprocesses: 11
 
-import time
-start_time = time.time()
-
+import random
 from mpi import MPI
-BCAST_MESSAGE = "Test message"
+
 mpi = MPI()
-init_time = time.time()
 
-BCAST_ROOT = 0
-
-print "Init time: %f" % (init_time-start_time)
-
+rank = mpi.MPI_COMM_WORLD.rank()
 size = mpi.MPI_COMM_WORLD.size()
-assert size > 2
 
-if mpi.MPI_COMM_WORLD.rank() == BCAST_ROOT:
+BCAST_ROOT = random.randint(0, size-1)
+BCAST_MESSAGE = "Test message for rank %d" % BCAST_ROOT
+
+if rank == BCAST_ROOT:
     mpi.MPI_COMM_WORLD.bcast(BCAST_ROOT, BCAST_MESSAGE)
 else:
     message = mpi.MPI_COMM_WORLD.bcast(BCAST_ROOT)
     print message
     assert message == BCAST_MESSAGE
 
-logic_time = time.time()
-
 mpi.finalize()
-
-finalize_time = time.time()
-
-print """
-TIME INFORMATION
-    Initialization: %f 
-    Logic: %f
-    Finalization: %f
-    Total: %f """ % (init_time - start_time, logic_time - init_time, finalize_time - logic_time, finalize_time - start_time) 
