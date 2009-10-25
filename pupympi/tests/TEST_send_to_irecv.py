a542f5cc1b22691c02f6402484d8bbfd46a4b5ce
Rune Bromer <rune.bromer@coniuro.dk>
2009-10-25 22:02:14 +0100

    Changed the test a bit, so it's a bit more readable.
    There is a strange problem with the test. It runs without problems 15 times
    when it's started with -c 2. But with anything larger it fails. It might be a
    problem where the messages from the full network startup messes around with
    the next messages.

--- pupympi/tests/TEST_send_to_irecv.py
+++ pupympi/tests/TEST_send_to_irecv.py
@@ -10,8 +10,10 @@ from mpi import MPI
 
 mpi = MPI()
 
-rank = mpi.MPI_COMM_WORLD.rank()
-size = mpi.MPI_COMM_WORLD.size()
+world = mpi.MPI_COMM_WORLD
+
+rank = world.rank()
+size = world.size()
 
 content = "This message was sent"
 DUMMY_TAG = 1
@@ -20,10 +22,8 @@ if rank == 0:
     # Send
     neighbour = 1
     print "Rank: %d sending to %d" % (rank,neighbour)
-    request = mpi.MPI_COMM_WORLD.send(neighbour,content,DUMMY_TAG)    
+    request = world.send(neighbour,content,DUMMY_TAG)    
     print "Rank: %d ALL DONE" % (rank)
-
-    
 elif rank == 1: # rank == 1
     # Waaaaait for it...
     neighbour = 0
@@ -31,9 +31,9 @@ elif rank == 1: # rank == 1
     # Recieve
     print "YAWN, rank: %d recieving from %d" % (rank,neighbour)
     #recieved = mpi.MPI_COMM_WORLD.recv(neighbour,DUMMY_TAG)    
-    request = mpi.MPI_COMM_WORLD.irecv(neighbour,DUMMY_TAG)    
-    recieved = request.wait()
-    print "Rank: %d RECIEVED %s" % (rank,recieved)
+    request = world.irecv(neighbour,DUMMY_TAG)    
+    received = request.wait()
+    print "Rank: %d RECIEVED %s" % (rank,received)
 else:
     print "I'm rank %d and I'm not doing anything in this test" % rank
 
