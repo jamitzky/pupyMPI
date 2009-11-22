253b9fee8b656598bf8d95f5c7a1af2cfaaec53a
hantho <fhantho@yahoo.com>
2009-11-22 18:16:01 +0100

    Upped the stakes for lock_trouble, we do 500 iterations now, and assert that the procs are not out of sync. 5000 iterations go fine also, but 500 should be proof enough for now. Also upped the time limit for big_messages i think.

--- pupympi/tests/TEST_lock_trouble.py
+++ pupympi/tests/TEST_lock_trouble.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python2.6
-# META: SKIP
-# heavier-duty test
+# meta-description: basic send and recive over many iterations where communicating parties should be in sync
+# meta-expectedresult: 0
+# meta-minprocesses: 2
+# meta-max_runtime: 20
 
 from mpi import MPI
 import time
+
 
 mpi = MPI()
 world = mpi.MPI_COMM_WORLD
 rank = world.rank()
 
 data = 50*"a"
-f = open("/tmp/mpi.local.rank%s.log" % rank, "w")
+DUMMY_TAG = 1
 
-max_iterations = 50
-    
+f = open("/tmp/mpi.lock_trouble.rank%s.log" % rank, "w")
+
+max_iterations = 500
 
 for iterations in xrange(max_iterations):
     if rank == 0: 
-        world.send(1, "rank%s,iterations%s" %(rank, iterations), 1)
+        world.send(1, "rank%s,iterations%s" %(rank, iterations), DUMMY_TAG)
         # print "%s: %s done sending" % (iterations, c_info.rank)
         msg =  "Iteration:%s for rank:%s \n" % (iterations, rank)
     elif rank == 1: 
-        recv = world.recv(0, 1)
+        recv = world.recv(0, DUMMY_TAG)
         # print "%s, %s done recv: %s" % (iterations, c_info.rank," str(recv)")
         msg =  "Iteration:%s for rank:%s got:%s \n" % (iterations, rank, recv)
     else:
@@ -31,11 +35,30 @@ for iterations in xrange(max_iterations):
     f.write(msg)
     f.flush()
     
+# Test that procs agree on last message (max_iterations)
+if rank == 0: 
+    world.send(1, "rank%s,iterations%s" %(rank, max_iterations), DUMMY_TAG)
+    # print "%s: %s done sending" % (iterations, c_info.rank)
+    msg =  "Last iteration: %s for rank:%s \n" % (max_iterations, rank)
+    f.write(msg)
+    f.flush()
+    
+elif rank == 1: 
+    recv = world.recv(0, DUMMY_TAG)
+    # print "%s, %s done recv: %s" % (iterations, c_info.rank," str(recv)")
+    msg =  "Last iteration:%s for rank:%s got:%s \n" % (max_iterations, rank, recv)
+    # We are only really interested in max_iterations here but the whole string should match also
+    goal = ("rank%s,iterations%s" %(0, max_iterations) )
+    assert recv == goal
+    f.write(msg)
+    f.flush()
+else:
+    pass
+
+    
 f.write( "Done for rank %d\n" % rank)
-#time.sleep(4)
-
-
 f.flush()
 f.close()
+
 mpi.finalize()
 
