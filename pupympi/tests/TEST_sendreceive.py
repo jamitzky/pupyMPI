e06c66bc40857ba9bacb82c32e07aa15b37f1841
hantho <fhantho@yahoo.com>
2009-11-24 02:02:31 +0100

    Notes on problem with sendrecieve

--- pupympi/tests/TEST_sendreceive.py
+++ pupympi/tests/TEST_sendreceive.py
@@ -2,11 +2,29 @@
 # meta-description: Testing the sendrecv call. Runs with odd no. of processes who pass a token around
 # meta-expectedresult: 0
 # meta-minprocesses: 5
+# meta-max_runtime: 90
 
 # This test is meant to be run with a odd number of processes
-# as of 22/11-09 this test has serious problems with more than 9 procs
+# With debugging on 13 procs can throw around the token in just under a minute, so if 5 can't do it in 1.5 minutes something is wrong
+"""
+as of 22/11-09 this test has serious problems with more than 9 procs. Often the
+first run is fine but even with a few seconds to cool of the next runs often hang
+
+- seems like sometimes a proccess recieves the token, but has not recieved all
+the start up messages from lower ranking processes. Therefore it has not left
+initialization and so does not pass on the token to the neighbour.
+Can it be that some of the lower ranks can actually manage to start up, recieve
+from lower, send off to all uppers and the recieve and pass on the token AND then
+shutdown sleeping for 4 secs and die and break socket before a higher gets the
+startup message?
+
+Sleep(9) does help a lot ... :/
+
+If we had a barrier we could test it
+"""
 
 from mpi import MPI
+import time
 
 mpi = MPI()
 
@@ -35,6 +53,11 @@ f.flush()
 f.write("Done for rank %d\n" % rank)
 f.flush()
 f.close()
+
+# Three seperate sleeps for superstition
+time.sleep(3)
+time.sleep(3)
+time.sleep(3)
 
 # Close the sockets down nicely
 mpi.finalize()
