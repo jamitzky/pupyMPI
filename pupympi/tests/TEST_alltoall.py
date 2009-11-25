ee4eecf0bc785529d4ac044f2e47e23573b94b4f
Rune Bromer <rune.bromer@coniuro.dk>
2009-11-25 12:33:05 +0100

    Correct use of the up_func and down_fucn. Currently the down_func is not used, but we should write that it's a quick way for each operation to provide with some cleanup method somewhere in the future

--- pupympi/tests/TEST_alltoall.py
+++ pupympi/tests/TEST_alltoall.py
@@ -13,6 +13,12 @@ recv_data = world.alltoall(send_data)
 
 expected_data = [ '%d --> %d' % (x, rank) for x in range(size)]
 
-assert sorted(recv_data) == sorted(expected_data)
+try:
+    assert sorted(recv_data) == sorted(expected_data)
+except AssertionError, e:
+    print "Got AssertionError. "
+    print "\tExcepted: %s" % expected_data
+    print "\tReceived data: %s" % recv_data
+    raise e
 
 mpi.finalize()
