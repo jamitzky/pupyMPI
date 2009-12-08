c3c759e43c5cd7b7ec868cae515dacbe6e95b0c7
hantho <fhantho@yahoo.com>
2009-12-09 00:42:32 +0100

    Work around for TEST_mmult dependency on numpy

--- pupympi/tests/TEST_mmult.py
+++ pupympi/tests/TEST_mmult.py
@@ -1,16 +1,27 @@
+#!/usr/bin/env python2.6
+# meta-description: Test funky matrix multiplication via allreduce
+# meta-expectedresult: 0
+
 from mpi import MPI
-from numpy import matrix
+    
 from mpi.operations import prod
 
-mpi = MPI()
+try:
+    from numpy import matrix
+except ImportError:
+    # NOTE: This test relies on Numpy... do we want to fail if not importable?
+    mpi = MPI()
+    mpi.finalize()
+else:
+    mpi = MPI()
+    
+    r = mpi.MPI_COMM_WORLD.rank()+1
+    
+    local_matrix = matrix( [[r, r, r], [r, r, r], [r, r, r] ])
+    
+    reduced_matrix = mpi.MPI_COMM_WORLD.allreduce(local_matrix, prod)
+    
+    if r == 1:
+        print reduced_matrix
 
-r = mpi.MPI_COMM_WORLD.rank()+1
-
-local_matrix = matrix( [[r, r, r], [r, r, r], [r, r, r] ])
-
-reduced_matrix = mpi.MPI_COMM_WORLD.allreduce(local_matrix, prod)
-
-if r == 1:
-    print reduced_matrix
-
-mpi.finalize()
+    mpi.finalize()
