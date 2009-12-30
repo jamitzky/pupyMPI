93e50ffff27a9878dc3282da2c5221d98406fa2a
Jan Wiberg <jan@lykke.org>
2009-12-30 14:47:20 +0100

    small change to pickle test

---
+++ pupympi/mpi/operations/__init__.py
@@ -0,0 +1,52 @@
+"""
+Operations for collective requests. These will take a
+list and do something and return the result. There are
+a number of settings you can set on the function itself
+that will control the behaviour.
+
+See the documentation for more information.
+"""
+
+def MPI_sum(input_list):
+    return sum(input_list)
+
+def prod(input_list):
+    """
+    Multiplies all the elements. The elements must be
+    integers, float etc. As an example the following code
+    uses allreduce to calculate the factorial function of n, 
+    where n is the size of the world communicator.::
+
+        from mpi import MPI
+        from mpi.operations import prod
+
+        mpi = MPI()
+
+        # We start n processes, and try to calculate n!
+        rank = mpi.MPI_COMM_WORLD.rank()
+        fact = mpi.MPI_COMM_WORLD.allreduce(rank, prod)
+
+        print "I'm rank %d and I also got the result %d. So cool" % (rank, fact)
+
+        mpi.finalize()
+    """
+    p = 1
+
+    # The input list is a list of dictionaries. We're only interested in
+    # looking through the actual values.
+
+    for e in input_list:
+        p *= e
+    return p
+
+prod.partial_data = True
+MPI_sum.partial_data = False
+
+def MPI_max(input_list):
+    """
+    Returns the largest element in the list. The input can be any kind of list
+    or pseudo list (string, tuple, array etc.).
+    
+    """
+
+    return max(input_list)
---
+++ pupympi/tests/TEST_bcast.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.6
+# meta-description: Test if mpi_bcast work. 
+# meta-expectedresult: 0
+# meta-minprocesses: 11
+
+import random
+from mpi import MPI
+
+mpi = MPI()
+
+world = mpi.MPI_COMM_WORLD
+
+rank = world.rank()
+size = world.size()
+
+BCAST_ROOT = 3
+BCAST_MESSAGE = "Test message for rank %d" % BCAST_ROOT
+
+if rank == BCAST_ROOT:
+    world.bcast(BCAST_ROOT, BCAST_MESSAGE)
+else:
+    message = world.bcast(BCAST_ROOT)
+    try:
+        assert message == BCAST_MESSAGE
+    except AssertionError, e:
+        #print "Expected data:", BCAST_MESSAGE
+        #print "Received data:", message
+        raise e
+mpi.finalize()
