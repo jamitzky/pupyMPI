02d14f9d777c248720fd9b4cdea10956d3918551
Jan Wiberg <jan@lykke.org>
2010-01-01 21:59:29 +0100

    Removed requirement for odd number of participants from sendrecv test

--- pupympi/tests/TEST_sendreceive.py
+++ pupympi/tests/TEST_sendreceive.py
@@ -16,7 +16,7 @@ rank = mpi.MPI_COMM_WORLD.rank()
 size = mpi.MPI_COMM_WORLD.size()
 
 
-assert size % 2 == 1 # Require odd number of participants
+#assert size % 2 == 1 # Require odd number of participants
 
 content = "conch"
 DUMMY_TAG = 1
