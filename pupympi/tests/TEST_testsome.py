feca85ee46dc8ddafcb8f5b14a5709f55b9ea4cb
Rune Bromer <rune.bromer@coniuro.dk>
2010-01-02 13:20:59 +0100

    Test case for testsome. Need example file for the end user documentation before it's 100% complete.
    ref #82

---
+++ pupympi/tests/TEST_testsome.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2.6
+# meta-description: Test case for testany operation. 
+# meta-expectedresult: 0
+# meta-minprocesses: 4
+
+from mpi import MPI
+from mpi import constants
+import time
+
+mpi = MPI()
+world = mpi.MPI_COMM_WORLD
+
+rank = world.rank()
+size = world.size()
+
+WAIT_FAST_ENOUGH = 0.05
+
+handles = []
+
+if rank != 0:
+    time.sleep(4)
+
+for i in range(100):
+    if rank == 0:
+        # This rank receives every message received by the other 
+        # processes. 
+        for j in range(size-1):
+            handle = world.irecv(constants.MPI_SOURCE_ANY) 
+            handles.append(handle)
+
+        while handles:
+            request_list = world.testsome(handles)
+            if request_list:
+                # Finish the request
+                start_time = time.time()
+                world.waitall(request_list)
+                end_time = time.time()
+
+                diff_time = end_time - start_time
+                assert diff_time < WAIT_FAST_ENOUGH * len(request_list)
+                handles = [ r for r in handles if r not in request_list]
+
+    else:
+        world.send(0, "My data", constants.MPI_TAG_ANY)
+
+mpi.finalize()
