73efbe5230fe15cca2411129dd25b7849ea621e3
Rune Bromer <rune.bromer@coniuro.dk>
2010-01-02 23:26:18 +0100

    It's now possible to scatter with more than N items in the array.. we'll do what we can to slice it

--- pupympi/tests/TEST_scatter.py
+++ pupympi/tests/TEST_scatter.py
@@ -11,14 +11,24 @@ rank = world.rank()
 size = world.size()
 
 SCATTER_ROOT = 3
-
 if rank == SCATTER_ROOT:
     scatter_data = range(size)
 else:
     scatter_data = None
 
 my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
+assert my_data == rank
+
+# ensure we can work with larger data
+if rank == SCATTER_ROOT:
+    scatter_data = range(size*10)
+else:
+    scatter_data = None
+
+my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
+expected_data = range(10*rank, 10*(rank+1))
+
+assert my_data == expected_data
 
 mpi.finalize()
 
-assert my_data == rank
