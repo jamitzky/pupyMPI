54ff6f6b723dbed6f088da123bbd46a41f31b551
hantho <fhantho@yahoo.com>
2010-01-02 16:48:21 +0100

    issend documentation now with nice example

---
+++ pupympi/tests/TEST_initialized.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.6
+# meta-description: MPI environment can be established
+# meta-expectedresult: 0
+# meta-minprocesses: 2
+
+import time
+from mpi import MPI
+
+assert not MPI.initialized(), "The mpi environment was initialized before starting mpi.. wrong"
+
+mpi = MPI()
+assert MPI.initialized(), "The mpi environment was not initialized after starting mpi.. wrong"
+
+# Close the sockets down nicely
+mpi.finalize()
+
