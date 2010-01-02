20086769161a18322e775ccb5f16f294c7999daa
Jan Wiberg <jan@lykke.org>
2010-01-02 22:41:32 +0100

    Documents the last exceptions. Fixes #85

--- pupympi/mpi/exceptions.py
+++ pupympi/mpi/exceptions.py
@@ -42,12 +42,12 @@ class MPIInvalidTagException(MPIException):
 
 class MPICommunicatorGroupNotSubsetOf(MPIException):
     """
-    Document me
+    Raised when a new communicator is created from a group that is not a subset of the parent communicator group.
     """
     pass
     
 class MPICommunicatorNoNewIdAvailable(MPIException):
     """
-    Document me
+    Raised if it is no longer possible to create new communicators because there are no unique identifiers available. This typically happens when communicators are created locally since only 31 can be created in total. 
     """
     pass
