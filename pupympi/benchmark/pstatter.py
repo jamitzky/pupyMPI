403cb34398b1e7f227fc461186e6eb9cdc82839b
Rune Bromer <rune.bromer@coniuro.dk>
2010-01-03 11:08:42 +0100

    Refactoring the operations module

---
+++ pupympi/benchmark/pstatter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.6
+
+import sys
+import pstats
+
+
+profileFile = sys.argv[1]
+
+p = pstats.Stats(profileFile)
+
+p.strip_dirs() # Remove superflous directory paths
+
+#p.sort_stats('time').print_stats(10)
+p.sort_stats('cum', 'time')
+p.print_callers(20)
+
+
---
+++ pupympi/tests/TEST_allreduce.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2.6
+# meta-description: allreduce, first computes factorial of mpi_size
+# meta-expectedresult: 0
+# meta-minprocesses: 10
+# todo: desc was:, then uses builtin max to find slowest process.
+
+from mpi import MPI
+from mpi.operations import MPI_prod
+from datetime import datetime
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+    
+mpi = MPI()
+world = mpi.MPI_COMM_WORLD
+n = datetime.now()
+
+# We start n processes, and try to calculate n!
+rank = world.rank()
+size = world.size()
+
+dist_fact = world.allreduce(rank+1, MPI_prod)
+
+try:
+    assert fact(size) == dist_fact
+except AssertionError, e:
+    #print "Got AssertionError"
+    #print "\tExpected data: %s" % fact(size)
+    #print "\tReceived: %s" % dist_fact
+    raise e
+
+mpi.finalize()
