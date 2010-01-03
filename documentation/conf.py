be2522863b4a4a33c87543802870bad1abfaa512
Rune Bromer <rune.bromer@coniuro.dk>
2010-01-03 20:17:47 +0100

    lame

--- documentation/conf.py
+++ documentation/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pupyMPI'
-copyright = u'2009, Jan Wiberg, Frederik Hanto & Rune Bromer'
+copyright = u'2009, Jan Wiberg, Frederik Hantho & Rune Bromer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
