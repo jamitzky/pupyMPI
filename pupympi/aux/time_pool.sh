cdfeb2c27d352a4f3dfacb25e07daeda69e16b96
Jan Wiberg <jan@lykke.org>
2010-01-03 14:52:29 +0100

    fix to pool script

--- pupympi/aux/time_pool.sh
+++ pupympi/aux/time_pool.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-time PYTHONPATH=. bin/mpirun.py -c 2 tests/TEST_finalize_quickly.py 
-time PYTHONPATH=. bin/mpirun.py -c 8 tests/TEST_finalize_quickly.py 
-time PYTHONPATH=. bin/mpirun.py -c 32 tests/TEST_finalize_quickly.py 
-time PYTHONPATH=. bin/mpirun.py -c 128 tests/TEST_finalize_quickly.py
-
-time PYTHONPATH=. bin/mpirun.py -c 2 tests/TEST_finalize_quickly.py --disable-full-network-startup
-time PYTHONPATH=. bin/mpirun.py -c 8 tests/TEST_finalize_quickly.py --disable-full-network-startup
-time PYTHONPATH=. bin/mpirun.py -c 32 tests/TEST_finalize_quickly.py --disable-full-network-startup
-time PYTHONPATH=. bin/mpirun.py -c 128 tests/TEST_finalize_quickly.py --disable-full-network-startup
+PYTHONPATH=.    time -f "2 full: %E" bin/mpirun.py -c 2 tests/TEST_finalize_quickly.py 
+PYTHONPATH=.    time -f "8 full: %E" bin/mpirun.py -c 8 tests/TEST_finalize_quickly.py 
+PYTHONPATH=.    time -f "32 full: %E" bin/mpirun.py -c 32 tests/TEST_finalize_quickly.py 
+PYTHONPATH=.    time -f "128 full: %E" bin/mpirun.py -c 128 tests/TEST_finalize_quickly.py
+                 
+PYTHONPATH=.    time -f "2 dyn: %E" bin/mpirun.py -c 2 tests/TEST_finalize_quickly.py --disable-full-network-startup
+PYTHONPATH=.    time -f "8 dyn: %E"  bin/mpirun.py -c 8 tests/TEST_finalize_quickly.py --disable-full-network-startup
+PYTHONPATH=.    time -f "32 dyn: %E"  bin/mpirun.py -c 32 tests/TEST_finalize_quickly.py --disable-full-network-startup
+PYTHONPATH=.    time -f "128 dyn: %E"  bin/mpirun.py -c 128 tests/TEST_finalize_quickly.py --disable-full-network-startup
