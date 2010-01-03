e4ef4fd6c88cb8f86e2dbc1ce80fa76375287fa2
hantho <fhantho@yahoo.com>
2010-01-03 11:05:24 +0100

    Rewriting

---
+++ pupympi/mpi/topology/BaseTopology.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+BaseTopology.py - 'abstract' master class for topologies
+"""
+
+import sys
+import os
+import unittest
+
+class BaseTopology:
+    def __init__(self):
+        pass
+
+
+class TopologyTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
