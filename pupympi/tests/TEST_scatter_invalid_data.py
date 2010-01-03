609235ce4f67dc3af412c616401689f180e158ad
Jan Wiberg <jan@lykke.org>
2010-01-03 15:35:27 +0100

    Automated merge with ssh://hg@bitbucket.org/bromer/python-mpi/

--- pupympi/tests/TEST_scatter_invalid_data.py
+++ pupympi/tests/TEST_scatter_invalid_data.py
@@ -1,6 +1,6 @@
-# meta-description: Scatter testing invalid data
+# meta-description: Scatter testing invalid data exceptions
 # meta-expectedresult: 0
-# meta-minprocesses: 10
+# meta-minprocesses: 4
 
 from mpi import MPI
 from mpi.exceptions import MPIException
@@ -11,15 +11,48 @@ world = mpi.MPI_COMM_WORLD
 rank = world.rank()
 size = world.size()
 
-SCATTER_ROOT = 3
+SCATTER_ROOT = 2
 
-scatter_data = range(size)
 
-try:
-    if rank != SCATTER_ROOT:
+scatter_data = 42 # Is unsliceable!
+
+if rank == SCATTER_ROOT: # Data type errors are only raised for root
+    try:
         world.scatter(scatter_data, root=SCATTER_ROOT)
+    except MPIException, e:
+        assert True
+    else:
+        # Didn't get expected exception
         assert False
-except MPIException:
+else:
     pass
+
+scatter_data = [42] # Is not sliceable enough!
+
+if rank == SCATTER_ROOT: # Data type errors are only raised for root
+    try:
+        world.scatter(scatter_data, root=SCATTER_ROOT)
+    except MPIException, e:
+        assert True
+    else:
+        # Didn't get expected exception
+        assert False
+else:
+    pass
+
+
+scatter_data = [42]*(size+1) # Is not finely sliceable
+
+if rank == SCATTER_ROOT: # Data type errors are only raised for root
+    try:
+        world.scatter(scatter_data, root=SCATTER_ROOT)
+    except MPIException, e:
+        assert True
+    else:
+        # Didn't get expected exception
+        assert False
+else:
+    pass
+
 
 mpi.finalize()
