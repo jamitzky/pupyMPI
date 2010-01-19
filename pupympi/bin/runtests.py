bf3a23105fad5154f38ab234b567f7b64424dd07
Jan Wiberg <jan@lykke.org>
2010-01-19 20:32:50 +0100

    Added GPLv2 license, along with GPL headers and copyright notice to primary files

--- pupympi/bin/runtests.py
+++ pupympi/bin/runtests.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python2.6
 # encoding: utf-8
+#
+# Copyright 2010 Rune Bromer, Frederik Hantho and Jan Wiberg
+# This file is part of pupyMPI.
+# 
+# pupyMPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# pupyMPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License 2
+# along with pupyMPI.  If not, see <http://www.gnu.org/licenses/>.
+#
 """
-runtests.py
-
-Created by Jan Wiberg on 2009-08-11.
-Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+runtests.py - test runner for pupympi
 """
 
 import sys
