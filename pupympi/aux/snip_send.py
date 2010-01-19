b0ca0ebfcc035489856d2bdba057261f08ff4e9d
Jan Wiberg <jan@lykke.org>
2010-01-19 18:45:35 +0100

    Cleaned repository of stale files from project phase

--- pupympi/aux/snip_send.py
+++
@@ -1,8 +0,0 @@
-from mpi import MPI
-mpi = MPI()
-
-if mpi.MPI_COMM_WORLD.rank() == 0:
-    mpi.MPI_COMM_WORLD.send(1, "Hello World!")
-else:
-    message = mpi.MPI_COMM_WORLD.recv(0)
-    print message
