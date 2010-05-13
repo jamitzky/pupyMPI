c7e41bc0e0522869dd7312347de2ffdf7efff0c8
Rune Bromer <rb@konstellation.dk>
2010-05-13 11:47:51 +0200

    Cleanup. Removed unused imports, functions and variable decl. Should run about 000.1% faster now

---
+++ pupympi/mpi/exceptions.py
@@ -0,0 +1,71 @@
+#
+# Copyright 2010 Rune Bromer, Frederik Hantho and Jan Wiberg
+# This file is part of pupyMPI.
+# 
+# pupyMPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# pupyMPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License 2
+# along with pupyMPI.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Define exception class
+class MPIException(Exception): 
+    """
+    General exception for pupyMPI. Look into the API at the
+    specific function for the actual cause of this exception. 
+    """
+    pass
+
+class NotImplementedException(MPIException):
+    pass
+    
+class MPITopologyException(MPIException): 
+    """
+    Custom exception for Topologies
+    """
+    pass
+
+class MPINoSuchRankException(MPIException):
+    """
+    Raised in calls where an invalid rank i supplied. 
+    """
+    pass
+
+class MPIInvalidStrideException(MPIException):
+    """
+    Custom exception for group (range) calls
+    """
+    pass
+
+class MPIInvalidRangeException(MPIException):
+    """
+    Custom exception for group (range) calls
+    """
+    pass
+
+class MPIInvalidTagException(MPIException):
+    """
+    Raised in calls where an invalid tag i supplied. Remember all
+    tags should be integers. See also the section about :ref:`TagRules`. 
+    """
+    pass
+
+class MPICommunicatorGroupNotSubsetOf(MPIException):
+    """
+    Raised when a new communicator is created from a group that is not a subset of the parent communicator group.
+    """
+    pass
+    
+class MPICommunicatorNoNewIdAvailable(MPIException):
+    """
+    Raised if it is no longer possible to create new communicators because there are no unique identifiers available. This typically happens when communicators are created locally since only 31 can be created in total. 
+    """
+    pass
---
+++ pupympi/tests/TEST_big_messages.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python2.6
+# meta-description: Tests how we deal with transmitting very large messages
+# meta-expectedresult: 0
+# meta-minprocesses: 2
+# meta-max_runtime: 60
+
+# NOTE: You do not want to call this test with debugging (that prints the message content) active!
+# Outputting the big messages take a _long_ time
+
+from mpi import MPI
+from mpi import constants
+
+mpi = MPI()
+
+# we expect the group of MPI_COMM_WORLD to match with the communicator itself
+rank = mpi.MPI_COMM_WORLD.rank()
+size = mpi.MPI_COMM_WORLD.size()
+
+# Log stuff so progress is easier followed
+f = open(constants.LOGDIR+"mpi.big_messages.rank%s.log" % rank, "w")
+
+# Test does not make sense for 1
+assert size > 1
+
+#### Setup prerequisites ####
+FIRST_TAG = 111
+SECOND_TAG = 222
+THIRD_TAG = 333
+
+string = "abcdefghijklmnopqrst" # 20 chars
+
+largeMsg = 256*string
+largerMsg = 1024*string
+heftyMsg = 1024*1024*string
+# You can make a string longer than this but python (pickle) gets in trouble, you can't repr it and it takes up a lot of RAM :)
+# max for python2.5 on my machine is about 350*1024*1024 chars.. let's stay below that
+
+#### Test message passing ####
+
+# One way comm: 0 sends, 1 recives, the rest do nothing
+if rank == 0:
+    mpi.MPI_COMM_WORLD.send(largeMsg, 1, FIRST_TAG)
+    mpi.MPI_COMM_WORLD.send(largerMsg, 1, SECOND_TAG)
+    mpi.MPI_COMM_WORLD.send(heftyMsg, 1, THIRD_TAG)    
+    f.write( "Done sending big messages - rank %d\n" % rank)
+    f.flush()
+elif rank == 1:
+    msg = mpi.MPI_COMM_WORLD.recv(0,FIRST_TAG)
+    assert msg == largeMsg
+    
+    msg = mpi.MPI_COMM_WORLD.recv(0,SECOND_TAG)
+    assert msg == largerMsg
+
+    msg = mpi.MPI_COMM_WORLD.recv(0,THIRD_TAG)
+    assert msg == heftyMsg
+    assert len(msg) == 1024*1024*20 # Check actual length
+    f.write( "Recieved all three big messages - rank %d\n" % rank)
+    f.flush()    
+else:
+    pass
+
+
+# Two way comm blocking
+if rank == 0:
+    mpi.MPI_COMM_WORLD.send(largerMsg, 1, SECOND_TAG)
+
+    msg = mpi.MPI_COMM_WORLD.recv(1,THIRD_TAG)
+    assert msg == heftyMsg
+    
+    msg = mpi.MPI_COMM_WORLD.recv(1,SECOND_TAG)
+    assert msg == largerMsg
+
+    mpi.MPI_COMM_WORLD.send(heftyMsg, 1, THIRD_TAG)
+    
+    f.write( "Blocking two-way passing of big messages done - rank %d\n" % rank)
+    f.flush()
+    
+elif rank == 1:
+    msg = mpi.MPI_COMM_WORLD.recv(0,SECOND_TAG)
+    assert msg == largerMsg
+
+    mpi.MPI_COMM_WORLD.send(heftyMsg, 1, THIRD_TAG)    
+    mpi.MPI_COMM_WORLD.send(largerMsg, 1, SECOND_TAG)
+
+    msg = mpi.MPI_COMM_WORLD.recv(0,THIRD_TAG)
+    assert msg == heftyMsg
+
+    f.write( "Blocking two-way passing of big messages done - rank %d\n" % rank)
+    f.flush()
+else:
+    pass
+
+
+# Two way comm immediate
+if rank == 0:
+    s1 = mpi.MPI_COMM_WORLD.isend(largerMsg, 1, SECOND_TAG)
+    s2 = mpi.MPI_COMM_WORLD.isend(heftyMsg, 1, THIRD_TAG)
+    
+    f.write( "Immediate send done - rank %d\n" % rank)
+    f.flush()
+    
+    r1 = mpi.MPI_COMM_WORLD.irecv(1,SECOND_TAG)
+    r2 = mpi.MPI_COMM_WORLD.irecv(1,THIRD_TAG)
+        
+    recieve1 = r1.wait()
+    recieve2 = r2.wait()
+
+    f.write( "Recv wait done - rank %d\n" % rank)
+    f.flush()
+    
+    assert recieve1 == largerMsg
+    assert recieve2 == heftyMsg
+
+    s1.wait()
+    s2.wait()
+
+    f.write( "Send wait done - rank %d\n" % rank)
+    f.flush()   
+elif rank == 1:
+    s1 = mpi.MPI_COMM_WORLD.isend(largerMsg, 0, SECOND_TAG)
+    s2 = mpi.MPI_COMM_WORLD.isend(heftyMsg, 0, THIRD_TAG)
+    
+    f.write( "Immediate send done - rank %d\n" % rank)
+    f.flush()
+    
+    r1 = mpi.MPI_COMM_WORLD.irecv(0,SECOND_TAG)
+    r2 = mpi.MPI_COMM_WORLD.irecv(0,THIRD_TAG)
+    
+    recieve1 = r1.wait()
+    recieve2 = r2.wait()
+
+    f.write( "Recv wait done - rank %d\n" % rank)
+    f.flush()
+    
+    assert recieve1 == largerMsg
+    assert recieve2 == heftyMsg
+
+    s1.wait()
+    s2.wait()
+
+    f.write( "Send wait done - rank %d\n" % rank)
+    f.flush()
+
+else:
+    pass
+
+
+f.write( "Done for rank %d\n" % rank)
+f.flush()
+f.close()
+
+# Close the sockets down nicely
+mpi.finalize()
