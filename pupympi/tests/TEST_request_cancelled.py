1c171ffaed560ac4a8957cf49f7ca282617bc30a
Asser Schr?der Fem? <asser@diku.dk>
2010-06-04 14:44:34 +0200

    Removed debug statements

---
+++ pupympi/tests/TEST_request_cancelled.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.6
+# meta-description: Test that irecv and isend request can be cancelled
+# meta-expectedresult: 0
+# meta-minprocesses: 2
+
+from mpi import MPI
+
+mpi = MPI()
+
+world = mpi.MPI_COMM_WORLD
+TAG = 1
+
+if world.rank() == 1:
+    req1 = world.isend( "My message 1", 0, TAG)
+
+    assert not req1.test_cancelled()
+    
+    req1.cancel()
+    
+    assert req1.test_cancelled()
+else:
+    req1 = world.irecv(1, TAG)
+
+    assert not req1.test_cancelled()
+    
+    req1.cancel()
+    
+    assert req1.test_cancelled()
+
+mpi.finalize()
