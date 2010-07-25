ee620fd02bda0a71ab417f24c4be2a16d9dfd2f0
Frederik Hantho <fhantho@yahoo.com>
2010-07-25 14:31:27 +0200

    a print statement slipped in

---
+++ pupympi/tests/TEST_initialized.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.6
+# meta-description: Test establishment of MPI environment
+# meta-expectedresult: 0
+# meta-minprocesses: 2
+
+from mpi import MPI
+
+assert not MPI.initialized(), "The mpi environment was initialized before starting mpi.. wrong"
+
+mpi = MPI()
+assert MPI.initialized(), "The mpi environment was not initialized after starting mpi.. wrong"
+
+# Close the sockets down nicely
+mpi.finalize()
+
