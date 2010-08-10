765ae53e492ee38a9983bfa4b62e55b3e5bf6048
Frederik Hantho <fhantho@yahoo.com>
2010-08-10 16:21:16 +0200

    Removed _comm_create_coll since it was used nowhere

---
+++ pupympi/tests/TEST_groups.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2.6
+# meta-description: Test simple groups functionality
+# meta-expectedresult: 0
+# meta-minprocesses: 2
+
+from mpi import MPI
+from mpi import constants
+
+mpi = MPI()
+
+# we expect the group of MPI_COMM_WORLD to match with the communicator itself
+rank = mpi.MPI_COMM_WORLD.rank()
+size = mpi.MPI_COMM_WORLD.size()
+
+#print "---> PROCESS %d/%d" % (rank,size)
+
+cwG = mpi.MPI_COMM_WORLD.group()
+#print "Group of MPI_COMM_WORLD: %s" % cwG
+
+assert rank is cwG.rank()
+assert size is cwG.size()
+
+newG = cwG.incl([rank])
+assert newG is not None
+#print "Incl group %s." % newG
+
+newG = cwG.excl([rank])
+assert newG is not None
+assert newG.rank() == -1
+
+newG = cwG.excl([0 if rank is 1 else 1])
+assert newG is not None
+#print "Excl group %s." % newG
+
+emptyG = cwG.incl([])
+assert emptyG is not None
+assert emptyG.size() is 0
+assert emptyG is constants.MPI_GROUP_EMPTY
+
+# Close the sockets down nicely
+mpi.finalize()
---
+++ pupympi/tests/TEST_reduce_lists.py
@@ -0,0 +1,46 @@
+# meta-description: Test reduce with list as main type
+# meta-expectedresult: 0
+# meta-minprocesses: 6
+"""
+This test tries the reduce operation with lists as the primary datatype.
+
+Every rank builds a list with rank elements. Reduce combined with min should
+find the smallest list. Combined with max we should find the biggest list.
+"""
+
+from mpi import MPI
+
+mpi = MPI()
+world = mpi.MPI_COMM_WORLD
+size = world.size()
+rank = world.rank()
+
+max_rank = size - 1
+reduce_root = 3 # Just so it's not always 0
+
+local_data = [ rank for x in range(rank) ]
+smallest_list = world.reduce(local_data,min,reduce_root)
+longest_list = world.reduce(local_data,max,reduce_root)
+
+
+expected_small = [] # The list from rank 0 should be the smallest
+expected_long = [max_rank for x in range(max_rank)] # The list from rank size-1 should be the longest
+
+if rank == reduce_root:
+    assert expected_small == smallest_list
+    assert expected_long == longest_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mpi.finalize()
