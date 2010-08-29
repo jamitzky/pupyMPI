763209229fdc3c78671e62528c2c95aa01b1629e
Frederik Hantho <fhantho@yahoo.com>
2010-08-29 02:23:37 +0200

    Fix stupid error in alltoall. Let alltoall take allkinds of sequences. Expanded alltoall test to include exotics.

--- pupympi/tests/TEST_alltoall.py
+++ pupympi/tests/TEST_alltoall.py
@@ -13,18 +13,40 @@ world = mpi.MPI_COMM_WORLD
 rank = world.rank()
 size = world.size()
 
+
+### HUMAN READABLE
 send_data = ["%d --> %d" % (rank, x) for x in range(size)]
-
 recv_data = world.alltoall(send_data)
-
 expected_data = [ '%d --> %d' % (x, rank) for x in range(size)]
-
 try:
     assert recv_data == expected_data
 except AssertionError, e:
-    #print "Got AssertionError. "
-    #print "\tExcepted: %s" % expected_data
-    #print "\tReceived data: %s" % recv_data
+    print "Got AssertionError. "
+    print "\tExpected: %s" % expected_data
+    print "\tReceived data: %s" % recv_data
+    raise e
+
+### FLOAT TUPLE
+send_data = tuple([ float(r/2) for r in range(size*2) ])
+recv_data = world.alltoall(send_data)
+expected_data = tuple([ float(rank) for _ in range(size*2)])
+assert recv_data == expected_data
+
+#### INT LIST
+send_data = [ r/3 for r in range(size*3) ]
+recv_data = world.alltoall(send_data)
+expected_data = [ rank for _ in range(size*3) ]
+assert recv_data == expected_data
+
+### STRING
+send_data = ''.join( [ str(r/2) for r in range(size*2) ] )
+recv_data = world.alltoall(send_data)
+expected_data = str(rank)*size*2
+try:
+    assert recv_data == expected_data
+except AssertionError, e:
+    print "\tExcepted: %s" % expected_data
+    print "\tReceived data: %s" % recv_data
     raise e
 
 mpi.finalize()
