9e6a448c83cc1cb21f8aa472ed65d17790b1da28
Rune Bromer <rb@konstellation.dk>
2010-09-05 15:47:46 +0200

    Added a migration script to convert benchmark data in the old format to the new format. Will convert a number of data in the next couple of commits, but the old data will stay in an archive folder.

---
+++ benchmark_data/migrate.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys, glob, re, shutil
+
+if __name__ == "__main__":
+    try:
+        from_folder = sys.argv[1]
+    except IndexError:
+        print "Run with a single argument: the folder to migrate"
+        sys.exit(1)
+    print "Migrating data for folder", from_folder
+
+    # Find the folders in the proper format
+    folders = glob.glob(from_folder + "single_*")
+    folders.extend (glob.glob(from_folder + "collective_*"))
+
+    folder_re = re.compile(".*(collective|single)_(\d+)")
+    date_re = re.compile(".*(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})\.csv")
+
+    for folder in folders:
+        match = folder_re.match(folder)
+        run_type, procs = match.groups()
+
+        # Find all the csv files
+        csv_files = glob.glob(folder + "/*.csv")
+
+        for csv_file in csv_files:
+            # locate the date from the file
+            date_match = date_re.match(csv_file)
+            if date_match:
+                date = date_match.groups()[0]
+
+                # Generate a new filename
+                to_file = "%spupymark.%s.%sprocs.4MB.%s.csv" % (from_folder, run_type[:4], procs, date)
+
+                # Copy the file to the new location
+                shutil.copyfile(csv_file, to_file)
+
+
+
