235a88cdc8184be19fd3b39c0a18d6e107211d3b
Asser Schr?der Fem? <asser@diku.dk>
2010-09-07 19:14:50 +0200

    Changed stuff in pupyprofplot

---
+++ pupympi/tests/TEST_scan.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.6
+# meta-description: Test that scan makes a partial reduce
+# meta-expectedresult: 0
+# meta-minprocesses: 10
+
+from mpi import MPI
+
+mpi = MPI()
+
+world = mpi.MPI_COMM_WORLD
+
+rank = world.rank()
+size = world.size()
+
+partial_sum = world.scan(rank, sum)
+
+#print "%d: Got partial sum of %d" % (rank, partial_sum)
+
+assert partial_sum == sum(range(rank+1))
+
+mpi.finalize()
