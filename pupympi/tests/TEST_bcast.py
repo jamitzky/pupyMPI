1f39719069dd8b2aa163d728690b746a32380841
Asser Schr?der Fem? <asser@diku.dk>
2010-09-07 17:40:23 +0200

    Added SOR benchmark data for pupy 0.6

---
+++ pupympi/tests/TEST_bcast.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.6
+# meta-description: Test if broadcast works
+# meta-expectedresult: 0
+# meta-minprocesses: 11
+
+from mpi import MPI
+
+mpi = MPI()
+
+world = mpi.MPI_COMM_WORLD
+
+rank = world.rank()
+size = world.size()
+
+BCAST_ROOT = 3
+BCAST_FIRST_MESSAGE = "Test message for rank %d" % BCAST_ROOT
+
+messages = [BCAST_FIRST_MESSAGE, None, "", -1]
+
+for msg in messages:
+    if rank == BCAST_ROOT:
+        world.bcast(msg, BCAST_ROOT)
+    else:
+        message = world.bcast(root=BCAST_ROOT)
+        try:
+            assert message == msg 
+        except AssertionError, e:
+            #print "="*80
+            #print "Expected", msg
+            #print "Received", message
+            #print "="*80
+            raise e
+
+mpi.finalize()
