c8efb56acdeb358b44856f8ca0cbcdf50e931300
Rune Bromer <rb@konstellation.dk>
2010-12-12 01:25:25 +0100

    Wrote a draft of the pack script. It needs to handle error messages from the packing server a bit better, but otherwise it is ready to go.

--- pupympi/mpi/migrate.py
+++ pupympi/mpi/migrate.py
@@ -3,7 +3,7 @@ from datetime import datetime
 class MigratePack(object):
     def __init__(self, mpi, socket_connection, bypassed_function):
         self.mpi = mpi
-        self.socket_connection
+        self.socket_connection = socket_connection
         self.success = False
 
         # The bypassed function is the one decorated with
@@ -15,12 +15,23 @@ class MigratePack(object):
 
         # This dict will be sent to the admin caller, who will
         # serialize it (probably with data from other ranks).
+        rank = mpi.MPI_COMM_WORLD.comm_group.rank()
         self.data = {
-            'rank' : mpi.MPI_COMM_WORLD.comm_group.rank(),
+            'rank' : rank,
             'meta' : {
                 'pack_start' : datetime.now(),
                 },
         }
+
+        # Serialize other data
+
+        # Close down what is needed to close down.
+
+        # Dump the session into a file.
+
+        # Send the data+file on the connection.
+        from mpi.network.utils import robust_send, prepare_message
+        robust_send(self.socket_connection, prepare_message(self.data, rank))
 
     def mpi_continue(self):
         """
