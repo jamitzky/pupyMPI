8cf2a65f59490fb0e81a8a1eb8a9c8c6d203c229
Rune Bromer <rb@konstellation.dk>
2010-12-18 13:43:35 +0100

    Added documentation to the pupy_readregister.py script and wrote an example like SOR solver.

---
+++ pupympi/bin/utils/pupy_readregisters.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2.6
+#
+# Copyright 2010 Rune Bromer, Asser Schroeder Femoe, Frederik Hantho and Jan Wiberg
+# This file is part of pupyMPI.
+#
+# pupyMPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# pupyMPI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 2
+# along with pupyMPI.  If not, see <http://www.gnu.org/licenses/>.
+#
+from utils import SendSimpleCommand, print_reports, parse_args
+from mpi import constants
+import sys
+
+def main():
+    ranks, hostinfo, bypass = parse_args()
+
+    # Test if we have a "simple" command. That is, we can handle it by simply
+    # sending a
+    try:
+        scmd_id = constants.CMD_READ_REGISTER
+
+        # Start a thread for each rand we want to send the command to.
+        threads = {}
+        for rank in ranks:
+            t = SendSimpleCommand(scmd_id, rank, hostinfo, bypass, pong=True, timeout=30)
+            t.start()
+            threads[rank] = t
+
+        reports = {}
+        for rank in threads:
+            t = threads[rank]
+            report, data = t.wait_until_ready()
+            if report:
+                reports[rank] = data
+            t.join()
+
+        for rank in reports:
+            print "---------------- Registers for rank %d -------------------" % rank
+            for key in reports[rank]:
+                d = reports[rank][key]
+                print "%s: %s" % (key, d)
+
+            print "----------------------------------------------------------"
+            print ""
+
+        sys.exit(0)
+
+    except KeyError:
+        pass
+
+    parser.error("Can't find that command. Did you miss spell something?")
+
+if __name__ == "__main__":
+    # Try to get around export pythonpath issue
+    main()
+
