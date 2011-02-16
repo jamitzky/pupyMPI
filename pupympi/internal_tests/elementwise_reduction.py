32af4bfbfa21256f18b64e311c3ca12f332e546f
Frederik Hantho <fhantho@yahoo.com>
2011-02-16 22:59:29 +0100

    Looks like we gain a bit from xrange and yet a bit from transforming the outer for loop into another list comprehension

--- pupympi/internal_tests/elementwise_reduction.py
+++ pupympi/internal_tests/elementwise_reduction.py
@@ -31,14 +31,15 @@ def simple(sequences, operation):
 
 def xsimple(sequences, operation):
     """
-    Original, no frills
+    using xrange instead of range seems to give a slight advantage
+    (note that this will disappear in python2.7 or 3.1 whenever they made range behave properly)
     """                
     reduced_results = []
     no_seq = len(sequences) # How many sequences
     seq_len = len(sequences[0]) # How long is a sequence
-    for i in range(seq_len):
+    for i in xrange(seq_len):
         try:
-            temp_list = [ sequences[m][i] for m in range(no_seq) ] # Temp list contains i'th element of each subsequence
+            temp_list = [ sequences[m][i] for m in xrange(no_seq) ] # Temp list contains i'th element of each subsequence
         except IndexError, e:
             # If any sequence is shorter than the first one an IndexError will be raised
             raise Exception("Whoops, seems like someone tried to reduce on uneven length sequences")
@@ -54,6 +55,28 @@ def xsimple(sequences, operation):
         reduced_results = tuple(reduced_results) # join
         
     return reduced_results
+
+def convoluted(sequences, operation):
+    """
+    all about the list comprehensions
+    """                
+    reduced_results = []
+    no_seq = len(sequences) # How many sequences
+    seq_len = len(sequences[0]) # How long is a sequence
+    
+    reduced_results = [ operation([ sequences[m][i] for m in xrange(no_seq) ]) for i in xrange(seq_len) ]
+    
+    # Restore the type of the sequence
+    if isinstance(sequences[0],str):
+        reduced_results = ''.join(reduced_results) # join char list into string
+    if isinstance(sequences[0],bytearray):
+        reduced_results = bytearray(reduced_results) # make byte list into bytearray
+    if isinstance(sequences[0],tuple):
+        reduced_results = tuple(reduced_results) # join
+        
+    return reduced_results
+
+
 
 def generate_data(bytemultiplier,participants):
     """
@@ -71,29 +94,38 @@ def generate_data(bytemultiplier,participants):
     
     return wholeset
 
-
 def runner(version):
     if version == 0:
        res = simple(testdata,min)
+    elif version == 1:
+        res = xsimple(testdata,min)
+    elif version == 2:
+        res = convoluted(testdata,min)
     else:
-        print "Hejhej"
-    
-    #res = reduce_elementwise(smallbase,min)
-    #print res
+        print "no version..."
+        
     
     
 if __name__=='__main__':
     # How big should the data payload be
-    bytemultiplier = 10000
+    bytemultiplier = 100
     # Participants (how wide is the payload)
     participants = 5
     # Generate the data
     global testdata
     testdata = generate_data(bytemultiplier,participants)
     
+    runs = 1000
+
     from timeit import Timer
     t_simple = Timer("runner(0)", "from __main__ import runner")
-    runs = 10
     duration = t_simple.timeit(runs)
     print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
     
+    t_xsimple = Timer("runner(1)", "from __main__ import runner")
+    duration = t_xsimple.timeit(runs)
+    print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
+
+    t_convoluted = Timer("runner(2)", "from __main__ import runner")
+    duration = t_convoluted.timeit(runs)
+    print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
