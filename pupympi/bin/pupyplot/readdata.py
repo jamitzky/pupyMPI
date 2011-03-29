b81353b6d54dfbdb72073a6978a0bbd729acd515
Rune Bromer <rune.bromer@coniuro.dk>
2011-03-29 16:32:00 +0200

    System writes a tagmapper file now

--- pupympi/bin/pupyplot/readdata.py
+++ pupympi/bin/pupyplot/readdata.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
             
     # Write the tags to a tagmapper
     tagmapper.write_tag_file(parse_folders, output_file)
-        
+
     # Insert the parsed data in the handle
     handle.dataobj.extend(parser.data)
     
