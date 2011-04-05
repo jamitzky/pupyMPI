d92c9719dc90460d65ebf8b88f1fb648aefdc2a8
Frederik Hantho <fhantho@yahoo.com>
2011-04-06 00:15:29 +0200

    Improved pickling tester. Still needs * more python types tested like Numpy arrays * other serializer methods (struct packing? dill?) * measurement of bytesize of serialized objects (transmission ain't free ya'heard)

--- pupympi/internal_tests/pickle_test.py
+++ pupympi/internal_tests/pickle_test.py
@@ -1,35 +1,76 @@
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
-from time import time
+"""
+Helper program to measure the cost of serializing
 
+Different serializers are timed and the size of the serialized output is reported
+"""
+
+import random
+import time
+import sys
+from contextlib import contextmanager
+
+import cPickle
+import pickle
+import marshal
+
+
+# Different objects to serialize
 class A():
     def __init__(self):
         self.a = 1
         self.b = 2
 
-d1 = (3, "A simple int")
-d2 = (range(10), "A medium list")
-d3 = (range(10000), "A large list")
 d4 = ([A() for x in range(100)], "100 objects")
-    
-for data_struct in (d1,d2,d3,d4):
-    data, data_desc = data_struct
-    print "\n"*5
-    print "="*80
-    for iterations in range(1000, 500000, 100000):
-        data = 'my test message'
-        n = time()
-        for i in range(iterations):
-            p = pickle.dumps({'rank' : 1, 'tag' : 1, 'data' : data, 'communicator' : 3})
-        t1 = time() - n
-        
-        n = time()
-        for i in range(iterations):
-            p = pickle.dumps((1, 1, data, 3))
-        t2 = time() - n
-        
-        f = float(t1)/float(t2)
-        print "Iterations %d, %s: tuple are %s times faster" % (iterations, data_desc, f)
-    print "="*80
+
+# Test objects and nice descriptions
+i1 = (42, "a simple small int")
+i2 = (sys.maxint, "a really big int")
+
+l1 = (range(10), "a small int list")
+l2 = (range(10000), "a large int list")
+
+# classify objects
+smalldata = [i1, i2, l1]
+bigdata = [l2]
+# proper repetition factors
+small = 100
+big = 1
+# scale it
+smalldata = [(a,b,small) for (a,b) in smalldata]
+bigdata = [(a,b,big) for (a,b) in bigdata]
+
+
+@contextmanager
+def timing(printstr="time", repetitions=0, swallow_exception=True):
+    start = time.time()
+    try:
+        yield
+    except Exception, e:
+        print "ERROR: " + str(e)
+        if not swallow_exception:
+            raise
+    finally:
+        total_time = time.time() - start
+        if repetitions > 0:
+            avg_time = total_time / repetitions
+            print "%s: %f / %f sec." % (printstr, total_time, avg_time)
+        else:
+            print "%s: %f sec." % (printstr, total_time)
+
+
+def runner(r = 100):
+    # Serializers to try
+    pickle_methods = [pickle, marshal, cPickle]
+
+    for serializer in pickle_methods:
+        for data, desc, scale in smalldata+bigdata:
+            repetitions = r * scale
+            with timing("%s dump+load reps:%i %s" % (serializer.__name__, repetitions,desc),repetitions):
+                for i in xrange(repetitions):
+                    s = serializer.dumps(data)
+                    l = serializer.loads(s)
+
+
+runner()
+
+
