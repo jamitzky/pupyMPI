72ba88ee92207f79144c6975a87721a879c61937
Rune Bromer <rune.bromer@coniuro.dk>
2011-04-17 14:11:12 +0200

    Commiting a version that will not work with allreduce. Please the latest tag instead.

--- pupympi/benchmark/collective.py
+++ pupympi/benchmark/collective.py
@@ -207,7 +207,7 @@ def test_Allreduce(size, max_iterations):
     t1 = ci.clock_function()
 
     # do magic
-    Allreduce(ci.data[:size], max_iterations)
+    Allreduce(ci.reduce_data[:size], max_iterations)
 
     t2 = ci.clock_function()
     time = t2 - t1
