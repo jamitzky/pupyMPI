9c2ca8a1be6531344c6b660d6ef0b8b279707ce0
Frederik Hantho <fhantho@yahoo.com>
2011-04-17 19:02:04 +0200

    Oh yeah, now numpy at least has a fighting chance. For few participants and various sizes we are almost within reach of zippy

--- pupympi/internal_tests/elementwise_reduction.py
+++ pupympi/internal_tests/elementwise_reduction.py
@@ -103,22 +103,85 @@ def zippy(sequences, operation):
 
     return reduced_results
 
-def nummy(sequences, operation):
-    print "type:%s type0:%s" % (type(sequences),type(sequences[0]))
-    m = numpy.matrix(sequences)
-
-    #print "type:%s type0:%s" % (type(m),type(m[0]))
-    res = m.min(0)
-    print "type:%s type0:%s" % (type(res),type(res[0]))
-    reduced_results = res.A[0]
+def mammy(sequences, operation):
+    """
+    mapping and zipping like there's no tomorrow,
+    but with special treatment for numpy arrays using matrices
+    """
+    #print "type:%s type0:%s" % (type(sequences),type(sequences[0]))
     
-    # This does not work since the matrix dimensions are preserved
-    #reduced_results = numpy.array(res)
-    print "type:%s type0:%s" % (type(reduced_results),type(reduced_results[0]))
+    if isinstance(sequences[0], numpy.ndarray):
+        m = numpy.matrix(sequences)
+        #print "type:%s type0:%s" % (type(m),type(m[0]))
+        res = m.min(0)
+        #print "type:%s type0:%s" % (type(res),type(res[0]))
+        reduced_results = res.A[0]
+    else:
+        reduced_results = map(operation,zip(*sequences))
+            
+    # Restore the type of the sequence
+    if isinstance(sequences[0],str):
+        reduced_results = ''.join(reduced_results) # join char list into string
+    if isinstance(sequences[0],bytearray):
+        reduced_results = bytearray(reduced_results) # make byte list into bytearray
+    if isinstance(sequences[0],tuple):
+        reduced_results = tuple(reduced_results) # join
+
+    #print "type:%s type0:%s" % (type(reduced_results),type(reduced_results[0]))
+    return reduced_results
+
+def mammy2(sequences, operation):
+    """
+    mapping and zipping like there's no tomorrow,
+    but with special treatment for numpy arrays using matrices
+    """
+    #print "type:%s type0:%s" % (type(sequences),type(sequences[0]))
+    
+    if isinstance(sequences[0], numpy.ndarray):
+        #print "type:%s type0:%s" % (type(m),type(m[0]))
+        #print "type:%s type0:%s" % (type(res),type(res[0]))
+        reduced_results = numpy.matrix(sequences).min(0).A[0]
+    else:
+        reduced_results = map(operation,zip(*sequences))
+            
+        # Restore the type of the sequence
+        if isinstance(sequences[0],str):
+            reduced_results = ''.join(reduced_results) # join char list into string
+        if isinstance(sequences[0],bytearray):
+            reduced_results = bytearray(reduced_results) # make byte list into bytearray
+        if isinstance(sequences[0],tuple):
+            reduced_results = tuple(reduced_results) # join
+
+    #print "type:%s type0:%s" % (type(reduced_results),type(reduced_results[0]))
+    return reduced_results
+
+def nummy(sequences, operation):
+    """
+    mapping and zipping like there's no tomorrow,
+    but with special treatment for numpy arrays
+    """
+    #print "type:%s type0:%s" % (type(sequences),type(sequences[0]))
+    if isinstance(sequences[0], numpy.ndarray):
+        #print "type:%s type0:%s" % (type(m),type(m[0]))
+        #print "type:%s type0:%s" % (type(res),type(res[0]))
+        reduced_results = numpy.minimum.reduce(sequences)
+    else:
+        reduced_results = map(operation,zip(*sequences))
+            
+        # Restore the type of the sequence
+        if isinstance(sequences[0],str):
+            reduced_results = ''.join(reduced_results) # join char list into string
+        if isinstance(sequences[0],bytearray):
+            reduced_results = bytearray(reduced_results) # make byte list into bytearray
+        if isinstance(sequences[0],tuple):
+            reduced_results = tuple(reduced_results) # join
+
+    #print "type:%s type0:%s" % (type(reduced_results),type(reduced_results[0]))
     return reduced_results
 
 def mappy(sequences, operation):
     """
+    NOTE: Mappy has been retired since it doesn't currently work
     mapping and zipping like there's no tomorrow
     """
     reduced_results = map(operation,*sequences)
@@ -153,10 +216,12 @@ def numpy_generate_data(bytemultiplier,participants):
     """
     Generate the dataset externally from measured functions so that impact is not measured
 
-    The bytemultiplier scales op the 50 char base string to appropriate size
+    The bytemultiplier scales op the 50 int base to appropriate size
     Participants represent the number of sequences to reduce on
     """
-    baserange = numpy.array(range(10))
+    baserange = numpy.arange(50)
+    
+    #baserange = numpy.arange(50,dtype=numpy.float64)
 
     wholeset = []
     for p in range(participants):
@@ -169,62 +234,78 @@ def numpy_generate_data(bytemultiplier,participants):
 
 def runner(version):
     if version == 0:
-       res = simple(testdata,min)
+       res = simple(testdata,MPI_min)
     elif version == 1:
-        res = xsimple(testdata,min)
+        res = xsimple(testdata,MPI_min)
     elif version == 2:
-        res = convoluted(testdata,min)
+        res = convoluted(testdata,MPI_min)
     elif version == 3:
         res = zippy(testdata,MPI_min)
     #elif version == 4:
     #    res = mappy(testdata,MPI_min)
     elif version == 4:
-        res = nummy(numpytestdata,MPI_min)
+        res = mammy(testdata,MPI_min)
+    elif version == 5:
+        res = mammy2(testdata,MPI_min)
+    elif version == 6:
+        res = nummy(testdata,MPI_min)
     else:
         print "no version..."
-
-    print res
+    
+    # DEBUG
+    #print res
 
 
 
 if __name__=='__main__':
     # How big should the data payload be
-    bytemultiplier = 2
+    bytemultiplier = 100
     # Participants (how wide is the payload)
-    participants = 4
+    #participants = 2
+    #participants = 5
+    #participants = 10
+    
+    #participants = 20       # Around 20 participants - nummy always wins
+    #participants = 40
     # Generate the data
     global testdata
-    global numpytestdata
-    testdata = generate_data(bytemultiplier,participants)
-    numpytestdata = numpy_generate_data(bytemultiplier,participants)
+    #testdata = generate_data(bytemultiplier,participants)
+    testdata = numpy_generate_data(bytemultiplier,participants)
     
-    testdata = numpytestdata
     # DEBUG
     #print numpytestdata
 
-    runs = 1
+    runs = 1000
+    
 
     from timeit import Timer
     t_simple = Timer("runner(0)", "from __main__ import runner")
     duration = t_simple.timeit(runs)
-    print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
+    print "simple \t\t took %f seconds meaning %f per call" % (duration, duration/runs)
 
     t_xsimple = Timer("runner(1)", "from __main__ import runner")
     duration = t_xsimple.timeit(runs)
-    print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
+    print "xsimple \t took %f seconds meaning %f per call" % (duration, duration/runs)
 
     t_convoluted = Timer("runner(2)", "from __main__ import runner")
     duration = t_convoluted.timeit(runs)
-    print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
+    print "convoluted \t took %f seconds meaning %f per call" % (duration, duration/runs)
 
     t_zippy = Timer("runner(3)", "from __main__ import runner")
     duration = t_zippy.timeit(runs)
-    print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
+    print "zippy \t\t took %f seconds meaning %f per call" % (duration, duration/runs)
 
-    t_nummy = Timer("runner(4)", "from __main__ import runner")
+    t_mammy = Timer("runner(4)", "from __main__ import runner")
+    duration = t_mammy.timeit(runs)
+    print "mammy \t\t took %f seconds meaning %f per call" % (duration, duration/runs)
+
+    t_mammy2 = Timer("runner(5)", "from __main__ import runner")
+    duration = t_mammy2.timeit(runs)
+    print "mammy2 \t\t took %f seconds meaning %f per call" % (duration, duration/runs)
+
+    t_nummy = Timer("runner(6)", "from __main__ import runner")
     duration = t_nummy.timeit(runs)
-    print "Test took %f seconds meaning %f per call" % (duration, duration/runs)
-
+    print "nummy \t\t took %f seconds meaning %f per call" % (duration, duration/runs)
 
     #t_mappy = Timer("runner(4)", "from __main__ import runner")
     #duration = t_mappy.timeit(runs)
