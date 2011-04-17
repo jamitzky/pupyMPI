0bc6d6acff7c69018eba59a16dbee9393d42b4bf
Rune Bromer <rune.bromer@coniuro.dk>
2011-04-17 16:28:28 +0200

    Reduce should also use the new data

--- pupympi/benchmark/collective.py
+++ pupympi/benchmark/collective.py
@@ -188,7 +188,7 @@ def test_Reduce(size, max_iterations):
     t1 = ci.clock_function()
 
     # do magic
-    Reduce(ci.data[:size], max_iterations)
+    Reduce(ci.reduce_data[:size], max_iterations)
 
     t2 = ci.clock_function()
     time = t2 - t1
