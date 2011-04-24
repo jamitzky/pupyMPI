6ec722a5c39a2cdd821cc4697d52017933381815
Frederik Hantho <fhantho@yahoo.com>
2011-04-25 01:09:43 +0200

    Added test of transmission of types both serialized and non serialized. Note that this test does not explicitly test that types are not serialized, for this you should check with debug output.

--- pupympi/tests/TEST_unpickled.py
+++ pupympi/tests/TEST_unpickled.py
@@ -4,6 +4,15 @@
 # meta-minprocesses: 2
 # meta-max_runtime: 25
 
+"""
+ISSUE: This test is to be used with debug output that shows the unpickled control
+        path being taken.
+        As a part of runtest it only goes to show that various dimensions of arrays
+        can be sent.
+        Consider folding the interesting array-sending into an existing numpy test
+        or try to find a hook into the raw data queue
+"""
+
 import numpy
 
 from mpi import MPI
@@ -60,5 +69,43 @@ if rank == 0:
 elif rank == 1:
     world.send(msg,0)
 
+# numpy 2D float array
+msg = numpy.array([[i,i*2,i * 1.0/3] for i in xrange(10)])
+if rank == 0:
+    received = world.recv(1)
+    assert numpy.alltrue(received == msg)
+    #print "expect: %s element-type:%s got: %s element-type:%s " % (msg, type(msg[0]), received, type(received[0]))
+elif rank == 1:
+    world.send(msg,0)
+
+# numpy 6D float array
+msg = numpy.arange(128).reshape(2,2,2,2,4,2)
+if rank == 0:
+    received = world.recv(1)
+    assert numpy.alltrue(received == msg)
+    #print "expect: %s element-type:%s got: %s element-type:%s " % (msg, type(msg[0]), received, type(received[0]))
+elif rank == 1:
+    world.send(msg,0)
+
+# numpy 3D elongated
+n = 2**24
+msg = numpy.arange((2*n*2),dtype=numpy.int32).reshape(2,n,2)
+if rank == 0:
+    received = world.recv(1)
+    assert numpy.alltrue(received == msg)
+    #print "expect: %s element-type:%s got: %s element-type:%s " % (msg, type(msg[0]), received, type(received[0]))
+elif rank == 1:
+    world.send(msg,0)
+
+# Bytearrays are also not serialized
+#msg = bytearray(range(128))
+import string
+msg = bytearray(string.letters)
+if rank == 0:
+    received = world.recv(1)
+    #print "expect l:%i element-type:%s slice:%s got l:%i element-type:%s slice:%s " % (len(msg), type(msg), msg[0:8], len(received), type(received), received[0:32])
+    assert received == msg
+elif rank == 1:
+    world.send(msg,0)
 
 mpi.finalize()
