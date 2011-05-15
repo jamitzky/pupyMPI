76824a8aa37b7757a225141b51bedb7c7287ad4e
Rune Bromer <rune.bromer@coniuro.dk>
2011-05-15 21:25:01 +0200

    Generate more plots. Faster and better

--- pupympi/bin/pupyplot/generate_all.sh
+++ pupympi/bin/pupyplot/generate_all.sh
@@ -18,8 +18,12 @@ do
 	mkdir plots/datasize_time/$n
 	
 	mkdir plots/datasize_time/$n/log
-	python line.py --x-axis-use-data-points --axis-x-type=log --axis-y-type=log $e --test-filter=:pingping,:pingpong --raw-filters=nodes:$n $1
+	python line.py --x-axis-use-data-points --axis-x-type=log --axis-y-type=log --test-filter=:pingping,:pingpong --raw-filters=nodes:$n $1
 	mv *.eps plots/datasize_time/$n/log
+
+	mkdir plots/datasize_time/$n/linlog
+	python line.py --x-axis-use-data-points --axis-x-type=log --axis-y-type=lin --test-filter=:pingping,:pingpong --raw-filters=nodes:$n $1
+	mv *.eps plots/datasize_time/$n/linlog
 
 	mkdir plots/datasize_time/$n/lin
 	python line.py --axis-y-type=lin $e --test-filter=:pingping,:pingpong --raw-filters=nodes:$n $1
@@ -29,38 +33,49 @@ done
 # Plot the ping pong and ping ping test
 
 echo "------------- Generating plots with nodes on x axis and time on y ------------"
-for t in "log" "lin"
-do
-	python line.py --axis-y-type=$t --test-filter=pingping,pingpong --raw-filters=nodes:2 $1
-	mv *.eps plots/datasize_time/2/$t
-done
+python line.py --axis-y-type=log --test-filter=pingping,pingpong --raw-filters=nodes:2 $1
+mv *.eps plots/datasize_time/2/log
+
+python line.py --axis-y-type=lin --test-filter=pingping,pingpong --raw-filters=nodes:2 $1
+mv *.eps plots/datasize_time/2/lin
+
+python line.py  --x-axis-use-data-points --axis-y-type=lin --axis-x-type=log --test-filter=pingping,pingpong --raw-filters=nodes:2 $1
+mv *.eps plots/datasize_time/2/linlog
+
 
 # Generate plots wth x=nodes, y=time for selected data sizes
 mkdir plots/nodes_time
 for d in 4 64 1024 2048 32768 2097152 4194304
 do
 	mkdir plots/nodes_time/$d
-	for t in "log" "lin"
-	do
-		mkdir plots/nodes_time/$d/$t
-		python line.py --raw-filters=datasize:$d --test-filter=:barrier,:pingping,:pingpong --axis-y-type=$t --series-column=none --x-data=nodes $1
-		if [ $? -eq 0 ]; then
-			mv *.eps plots/nodes_time/$d/$t
-		else
-			echo "Prolem with the following plot command"
-			echo "python line.py --raw-filters=datasize:$d --test-filter=:barrier,:pingping,:pingpong --axis-y-type=$t --series-column=none --x-data=nodes $1"
-		
-		fi
-	done
+
+	mkdir plots/nodes_time/$d/log
+	python line.py --raw-filters=datasize:$d --test-filter=:barrier,:pingping,:pingpong --axis-y-type=log --axis-x-type=lin --x-axis-use-data-points  --series-column=none --x-data=nodes $1
+	mv *.eps plots/nodes_time/$d/log
+
+	mkdir plots/nodes_time/$d/lin
+	python line.py --raw-filters=datasize:$d --test-filter=:barrier,:pingping,:pingpong --axis-y-type=lin --axis-x-type=lin --x-axis-use-data-points  --series-column=none --x-data=nodes $1
+	mv *.eps plots/nodes_time/$d/lin
+
+	mkdir plots/nodes_time/$d/linlog
+	python line.py --raw-filters=datasize:$d --test-filter=:barrier,:pingping,:pingpong --axis-y-type=lin --axis-x-type=log --x-axis-use-data-points  --series-column=none --x-data=nodes $1
+	mv *.eps plots/nodes_time/$d/linlog
+	
+	
 done	
 
 # Handle barrier manually.
 mkdir plots/nodes_time/barrier/
-for t in "log" "lin"
-	do
-		python line.py --test-filter=barrier --axis-y-type=$t --series-column=none --x-data=nodes $1
-		mv Barrier.eps plots/nodes_time/barrier/$t.eps
-	done
+
+python line.py --test-filter=barrier --axis-y-type=lin --axis-x-type=lin  --x-axis-use-data-points --series-column=none --x-data=nodes $1
+mv Barrier.eps plots/nodes_time/barrier/lin.eps
+
+python line.py --test-filter=barrier --axis-y-type=lin --axis-x-type=log  --x-axis-use-data-points --series-column=none --x-data=nodes $1
+mv Barrier.eps plots/nodes_time/barrier/linlog.eps
+
+python line.py --test-filter=barrier --axis-y-type=log --axis-x-type=log  --x-axis-use-data-points --series-column=none --x-data=nodes $1
+mv Barrier.eps plots/nodes_time/barrier/log.eps
+
 
 # Clean up if there are anything left from the runs
 python cleanup.py
