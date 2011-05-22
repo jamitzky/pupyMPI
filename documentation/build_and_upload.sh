be7d517a2655c7a20b8f7a4120e0efdce393d4b4
Rune Bromer <rune.bromer@coniuro.dk>
2011-05-22 16:00:06 +0200

    Removed warnings from sphinx. We need to go through the documentation and make sure it is setup nice

--- documentation/build_and_upload.sh
+++ documentation/build_and_upload.sh
@@ -1,5 +1,7 @@
 make clean
 PYTHONPATH=../pupympi/ make html
 mv _build/html pupympi
+chmod a+r pupympi
 scp -r pupympi/ bromer@tyr.diku.dk:www/
+ssh bromer@tyr.diku.dk chmod -R 755 www/pupympi
 rm -rf pupympi
