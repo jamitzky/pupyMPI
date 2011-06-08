b0c1c2507e937e462fdf4efde6302bd2da50128d
Frederik Hantho <fhantho@yahoo.com>
2011-06-09 01:20:55 +0200

    scatter now works with multi-dimensional arrays. Still buggin on root swap. Something odd in the topology?

--- pupympi/tests/TEST_scatter_types.py
+++ pupympi/tests/TEST_scatter_types.py
@@ -11,7 +11,7 @@ world = mpi.MPI_COMM_WORLD
 rank = world.rank()
 size = world.size()
 
-SCATTER_ROOT = 2
+SCATTER_ROOT = 3
 # DEBUG
 #SCATTER_ROOT = 0
 
@@ -24,32 +24,42 @@ SCATTER_ROOT = 2
 #my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
 #assert my_data == [[rank,rank]]
 
-# bytearray
-chunksize = 4
-import string
-# set it up so rank 0 gets 'a'*chunksize rank 1 gets 'b'*chunksize and so on
-basebytes = bytearray( ''.join([l*chunksize for l in string.ascii_letters[:size]]) )
-
-if rank == SCATTER_ROOT:    
-    scatter_data = basebytes
-else:
-    scatter_data = None
-
-my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
-assert numpy.alltrue(my_data == bytearray( string.ascii_letters[rank]*chunksize ) )
-
-## Numpy array
-#chunksize = 5
+## bytearray
+#chunksize = 4
+#import string
+## set it up so rank 0 gets 'a'*chunksize rank 1 gets 'b'*chunksize and so on
+#basebytes = bytearray( ''.join([l*chunksize for l in string.ascii_letters[:size]]) )
+#
 #if rank == SCATTER_ROOT:    
-#    scatter_data = numpy.arange(size*chunksize)
+#    scatter_data = basebytes
 #else:
 #    scatter_data = None
 #
 #my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
+#assert numpy.alltrue(my_data == bytearray( string.ascii_letters[rank]*chunksize ) )
+
+# Numpy array
+chunksize = 5
+if rank == SCATTER_ROOT:    
+    scatter_data = numpy.arange(size*chunksize)
+else:
+    scatter_data = None
+
+my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
 #assert numpy.alltrue(my_data == numpy.arange(rank*chunksize,(rank+1)*chunksize))
-##print "rank %i got %s" % (rank, my_data)
+print "rank %i got %s" % (rank, my_data)
 
 
+## multidimensional Numpy float arrays
+#chunksize = 3
+#if rank == SCATTER_ROOT:
+#    #2*3*2 ints of 8 bytes = 96 bytes = 48 bytes per proc
+#    scatter_data = numpy.arange(size*chunksize*2*5).reshape(size*2,chunksize,5)
+#else:
+#    scatter_data = None
+#my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
+#assert numpy.alltrue(my_data == numpy.arange(rank*chunksize*2*5,(rank+1)*chunksize*2*5).reshape(2,chunksize,5) )
+#
 ## 4D Numpy float array
 #if rank == SCATTER_ROOT:    
 #    scatter_data = numpy.arange(size*chunksize*12).reshape(size*3,chunksize,2,2)
@@ -57,8 +67,7 @@ assert numpy.alltrue(my_data == bytearray( string.ascii_letters[rank]*chunksize 
 #    scatter_data = None
 #
 #my_data = world.scatter(scatter_data, root=SCATTER_ROOT)
-##print "rank:%i got:%s shape:%s" % (rank, my_data, my_data.shape)
-#assert numpy.alltrue(my_data == numpy.arange(rank*chunksize*12,(rank+1)*chunksize*12).reshape(3,5,2,2) )
+#assert numpy.alltrue(my_data == numpy.arange(rank*chunksize*12,(rank+1)*chunksize*12).reshape(3,chunksize,2,2) )
 
 mpi.finalize()
 
