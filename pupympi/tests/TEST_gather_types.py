180645ee93b1cb2dc59d8bf9682454bae3106609
Frederik Hantho <fhantho@yahoo.com>
2011-06-09 18:43:36 +0200

    gather now works for pickless 1D arrays with root swap. Still need to include bytearrays and multidimensional numpy arrays.

--- pupympi/tests/TEST_gather_types.py
+++ pupympi/tests/TEST_gather_types.py
@@ -13,14 +13,31 @@ world = mpi.MPI_COMM_WORLD
 rank = world.rank()
 size = world.size()
 
-# Every processes sends rank to the root
 ROOT = 2
+#ROOT = 0
 
-# Test numpy arrays everyone sends an array of three elements
-na = numpy.arange(rank,rank+3)
+### gather an int
+#received = world.gather(rank, root=ROOT)
+#if ROOT == rank:
+#    assert received == range(size)
+#else:
+#    assert received == None
+#
+### gather a string
+#received = world.gather("im the real rank:%i"%rank, root=ROOT)
+#if ROOT == rank:
+#    assert received == [ "im the real rank:%i"%r for r in range(size) ]
+#else:
+#    assert received == None
+#
+
+# Test numpy arrays everyone sends an array of chunksize elements
+chunksize = 3
+na = numpy.arange(rank,rank+chunksize)
 received = world.gather(na, root=ROOT)
 if ROOT == rank:
-    #assert received == [numpy.arange(r,r+3) for r in range(size)]
+    assert numpy.all( numpy.array(received) == numpy.array([numpy.arange(r,r+chunksize) for r in range(size)]) )
+    #received = [numpy.arange(r,r+chunksize) for r in range(size)]
     print "Rank:%i received:%s" % (rank, received)
 else:
     #assert received == None
