9d66b629f0d2b79d74578f3f882d6615215833d7
Frederik Hantho <fhantho@yahoo.com>
2011-06-13 16:03:28 +0200

    get all tests going in allgather_types test

--- pupympi/tests/TEST_allgather_types.py
+++ pupympi/tests/TEST_allgather_types.py
@@ -15,11 +15,11 @@ rank = world.rank()
 size = world.size()
 
 
-#### Test simple bytearray - everyone sends an array of chunksize elements
-#chunksize = 5
-#ba = bytearray(range(rank,rank+chunksize))
-#received = world.allgather(ba)
-#assert received == [bytearray(range(r,r+chunksize)) for r in range(size)]
+### Test simple bytearray - everyone sends an array of chunksize elements
+chunksize = 5
+ba = bytearray(range(rank,rank+chunksize))
+received = world.allgather(ba)
+assert received == [bytearray(range(r,r+chunksize)) for r in range(size)]
 
 
 ### Test simple numpy array - everyone sends an array of chunksize elements
@@ -29,13 +29,13 @@ received = world.allgather(na)
 assert numpy.all( numpy.array(received) == numpy.array([numpy.arange(r,r+chunksize) for r in range(size)]) )
 
 
-#### Test multidimensional numpy arrays - everyone sends an array of size*chunksize*2 elements
-#chunksize = 4
-#na = numpy.arange(rank*100,rank*100+size*chunksize*2).reshape(size*2,chunksize)
-#received = world.allgather(na)
-#
-#control = numpy.array([numpy.arange(100*r,100*r+size*chunksize*2).reshape(size*2,chunksize) for r in range(size)]) 
-#assert numpy.all( numpy.array(received) == control)
+### Test multidimensional numpy arrays - everyone sends an array of size*chunksize*2 elements
+chunksize = 4
+na = numpy.arange(rank*100,rank*100+size*chunksize*2).reshape(size*2,chunksize)
+received = world.allgather(na)
+
+control = numpy.array([numpy.arange(100*r,100*r+size*chunksize*2).reshape(size*2,chunksize) for r in range(size)]) 
+assert numpy.all( numpy.array(received) == control)
 
 
 mpi.finalize()
